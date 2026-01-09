7be30648aa6444967e832a54b99a8dbf91e41327
RiddhiSharmaa <Riddhisharma9900@gmail.com>
2026-01-09 23:15:09 +0530

    Time: 55 ms (69.26%) | Memory: 21.3 MB (43.88%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -3,6 +3,7 @@ public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
         unordered_set<string>words(wordList.begin(), wordList.end());
         queue<pair<string, int>>q;
+
         q.push({beginWord, 1});
         if (words.count(beginWord)) words.erase(beginWord);
 
@@ -12,11 +13,9 @@ public:
             q.pop();
             
             for (int j = 0; j < word.length(); j++){
-                for (int i = 0; i < 26; i++){
-                    string w1  = word.substr(0, j);
-                    char w2 = char('a' + i);
-                    string w3 = (j + 1 < word.length()) ? word.substr(1 + j, word.length() - j - 1) : "";
-                    string replacedWord = w1 + w2 + w3;
+                string replacedWord = word;
+                for (char ch = 'a'; ch <= 'z'; ch++){
+                    replacedWord[j] = ch;
                     
                     if (words.count(replacedWord)){
                         if (replacedWord == endWord){
