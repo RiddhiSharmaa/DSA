510fb32819a70d1f3bb4650d0999a9859dca334e
RiddhiSharmaa <Riddhisharma9900@gmail.com>
2026-01-09 23:12:04 +0530

    Time: 176 ms (33.15%) | Memory: 21.2 MB (58.80%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -4,11 +4,11 @@ public:
         unordered_set<string>words(wordList.begin(), wordList.end());
         queue<pair<string, int>>q;
         q.push({beginWord, 1});
+        if (words.count(beginWord)) words.erase(beginWord);
 
         while (!q.empty()){
             string word = q.front().first;
             int level = q.front().second;
-            words.erase(word);
             q.pop();
             
             for (int j = 0; j < word.length(); j++){
