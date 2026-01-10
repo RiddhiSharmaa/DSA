0158879dd1c80817922e7940811f4a6de63d6e06
RiddhiSharmaa <Riddhisharma9900@gmail.com>
2026-01-11 01:13:20 +0530

    Time: 82 ms (26.64%) | Memory: 98.8 MB (14.74%) - LeetSync

---
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        if (!k || nums.size() == 1) return false;
+        unordered_map<int, int>mpp;
+        int left = 0, WindowSize = INT_MAX;
+        mpp[nums[left]]++;
+
+        for (int right = 1; right < nums.size(); right++){
+            mpp[nums[right]]++;
+
+            if ((right - left) > k){
+                mpp[nums[left]]--;
+                left++;
+            }
+
+            while (mpp[nums[right]] > 1){
+                if (nums[left] == nums[right] && ((right - left) <= k)){
+                    return true;
+                }
+                mpp[nums[left]]--;
+                left++;
+            }
+
+            if (nums[left] == nums[right] && ((right - left) <= k)){
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
