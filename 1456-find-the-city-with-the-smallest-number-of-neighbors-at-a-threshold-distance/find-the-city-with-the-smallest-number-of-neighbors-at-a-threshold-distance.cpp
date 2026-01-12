80d715cf7f57c2f476f3963781d832088fe4a6e4
RiddhiSharmaa <Riddhisharma9900@gmail.com>
2026-01-12 18:07:57 +0530

    Time: 249 ms (5.03%) | Memory: 16.5 MB (94.19%) - LeetSync

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,48 +1,93 @@
 class Solution {
 public:
-    // By Floyd Warshall
+    // By Bellman Ford
+    int bellmanFord(int node, int n, vector<vector<int>>& edges, 
+    int &distanceThreshold){
+        int cnt = 0;
+        vector<int>dist(n, 1e9);
+        dist[node] = 0;
+
+        for (int i = 0; i < n-1; i++){
+            for (auto& edge : edges){
+                int u = edge[0];
+                int v = edge[1];
+                int w = edge[2];
+
+                if (dist[u] != 1e9 && w + dist[u] < dist[v]){
+                    dist[v] = w + dist[u];
+                }
+                
+                // belman ford is only for directed graphs, hence for undirected 
+                // we have to make a - b => (a -> b) && (b -> a)
+                if (dist[v] != 1e9 && w + dist[v] < dist[u]){
+                    dist[u] = w + dist[v]; 
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++){
+            if (dist[i] <= distanceThreshold) cnt++;
+        }
+
+        return cnt;
+    }
     int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
-        vector<vector<int>>dist(n, vector<int>(n,1e9));
-        int cnt = INT_MAX;
+        int minCnt = distanceThreshold;
         int ans = 0;
 
-        for (auto& edge : edges){
-            int u = edge[0];
-            int v = edge[1];
-            int w = edge[2];
-            dist[u][v] = w;
-            dist[v][u] = w;
-        }
-
         for (int i = 0; i < n; i++){
-            for (int j = 0; j < n; j++){
-                if (i == j) dist[i][j] = 0;
-            }
-        }
-
-        for (int via = 0; via < n; via++){
-            for (int i = 0; i < n; i++){
-                for (int j = 0; j < n; j++){
-                    dist[i][j] = min(dist[i][j], dist[i][via] + dist[via][j]);
-                }
-            }
-        }
-
-        for (int i = 0; i < n; i++){
-            int rowWiseCount = 0;
-            for (int j = 0; j < n; j++){
-                if (dist[i][j] <= distanceThreshold){
-                    rowWiseCount++;
-                }
-            }
-            if (rowWiseCount <= cnt){
-                cnt = rowWiseCount;
+            int getCnt = bellmanFord(i, n, edges, distanceThreshold);
+            if (getCnt <= minCnt){
+                minCnt = getCnt;
                 ans = i;
             }
         }
 
         return ans;
     }
+    // By Floyd Warshall
+    // int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
+    //     vector<vector<int>>dist(n, vector<int>(n,1e9));
+    //     int cnt = INT_MAX;
+    //     int ans = 0;
+
+    //     for (auto& edge : edges){
+    //         int u = edge[0];
+    //         int v = edge[1];
+    //         int w = edge[2];
+    //         dist[u][v] = w;
+    //         dist[v][u] = w;
+    //     }
+
+    //     for (int i = 0; i < n; i++){
+    //         for (int j = 0; j < n; j++){
+    //             if (i == j) dist[i][j] = 0;
+    //         }
+    //     }
+
+    //     for (int via = 0; via < n; via++){
+    //         for (int i = 0; i < n; i++){
+    //             for (int j = 0; j < n; j++){
+    //                 dist[i][j] = min(dist[i][j], dist[i][via] + dist[via][j]);
+    //             }
+    //         }
+    //     }
+
+    //     for (int i = 0; i < n; i++){
+    //         int rowWiseCount = 0;
+    //         for (int j = 0; j < n; j++){
+    //             if (dist[i][j] <= distanceThreshold){
+    //                 rowWiseCount++;
+    //             }
+    //         }
+    //         if (rowWiseCount <= cnt){
+    //             cnt = rowWiseCount;
+    //             ans = i;
+    //         }
+    //     }
+
+    //     return ans;
+    // }
 
     // By Djisktra
     // int djikstra(int node, vector<vector<pair<int, int>>>& adj, 
