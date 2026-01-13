ce204c747b5c6728b947551bfa416b671c20d78b
RiddhiSharmaa <Riddhisharma9900@gmail.com>
2026-01-13 17:48:59 +0530

    Time: 7 ms (66.05%) | Memory: 13 MB (73.76%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.cpp
+++ 794-swim-in-rising-water/swim-in-rising-water.cpp
@@ -3,37 +3,36 @@ public:
     typedef pair<int, pair<int, int>> P;
     int swimInWater(vector<vector<int>>& grid) {
         int n = grid.size();
-        vector<vector<int>>time(n, vector<int>(n, 1e9));
         vector<vector<int>>visited(n, vector<int>(n, 0));
+        vector<vector<int>>time(n, vector<int>(n, 1e9));
         priority_queue<P, vector<P>, greater<P>>pq;
+
+        vector<int>rows = {-1, 0, 1, 0};
+        vector<int>cols = {0, 1, 0 , -1};
 
         pq.push({grid[0][0], {0, 0}});
         time[0][0] = grid[0][0];
 
-        vector<int>rows =  {-1, 0, 1, 0};
-        vector<int>cols = {0, 1, 0, -1};
-
         while (!pq.empty()){
-            int row = pq.top().second.first;
-            int col = pq.top().second.second;
+            int x = pq.top().second.first;
+            int y = pq.top().second.second;
             int t = pq.top().first;
             pq.pop();
 
-            if (t > time[row][col]) continue;
-            if (row == n-1 && col == n-1) return t;
-            visited[row][col] = 1;
-            
+            if (x == n-1 && y == n-1) return t;
+            if (visited[x][y]) continue;
+            visited[x][y] = 1;
+
             for (int i = 0; i < 4; i++){
-                int r = row + rows[i];
-                int c = col + cols[i];
+                int r = x + rows[i];
+                int c = y + cols[i];
 
                 if (r >= 0 && r < n && c >= 0 && c < n && !visited[r][c]){
-                    int newTime = max(t, grid[r][c]);
-                    if (newTime < time[r][c]) {
-                        time[r][c] = newTime;
-                        pq.push({newTime, {r, c}});
+                    int maxTime = max(t, grid[r][c]);
+                    if (maxTime < time[r][c]){
+                        time[r][c] = maxTime;
+                        pq.push({maxTime, {r, c}});
                     }
-
                 }
             }
         }
