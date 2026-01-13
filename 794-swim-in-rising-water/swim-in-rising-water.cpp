4cd0f697bdfdf72d1e81d0d076acc7bbf7a5a7b6
RiddhiSharmaa <Riddhisharma9900@gmail.com>
2026-01-13 17:01:42 +0530

    Time: 1 ms (93.77%) | Memory: 13 MB (73.76%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.cpp
+++ 794-swim-in-rising-water/swim-in-rising-water.cpp
@@ -1,36 +1,41 @@
 class Solution {
 public:
+    typedef pair<int, pair<int, int>> P;
     int swimInWater(vector<vector<int>>& grid) {
         int n = grid.size();
-        vector<vector<int>> time(n, vector<int>(n, 1e9));
-        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>,
-        greater<pair<int, pair<int, int>>>>pq;
+        vector<vector<int>>time(n, vector<int>(n, 1e9));
+        vector<vector<int>>visited(n, vector<int>(n, 0));
+        priority_queue<P, vector<P>, greater<P>>pq;
 
         pq.push({grid[0][0], {0, 0}});
         time[0][0] = grid[0][0];
-        vector<int>row = {-1, 0, 1, 0};
-        vector<int>col = {0, 1, 0, -1};
+
+        vector<int>rows =  {-1, 0, 1, 0};
+        vector<int>cols = {0, 1, 0, -1};
 
         while (!pq.empty()){
-            auto node = pq.top().second;
+            int row = pq.top().second.first;
+            int col = pq.top().second.second;
             int t = pq.top().first;
             pq.pop();
 
-            if (t > time[node.first][node.second]) continue;
-
-            if (node.first == n-1 && node.second == n-1) return t;
-
-            for (int i = 0; i < 4; i++){
-                int r = row[i] + node.first;
-                int c = col[i] + node.second;
-               
-                if (r >= 0 && r < grid.size() && c >= 0 && c < grid[0].size() && 
-                max(t, grid[r][c]) < time[r][c]){
-                    time[r][c] = max(t, grid[r][c]);
-                    pq.push({time[r][c], {r, c}});
-                }                
-            }
+            if (t > time[row][col]) continue;
+            if (row == n-1 && col == n-1) return t;
+            visited[row][col] = 1;
             
+            for (int i = 0; i < 4; i++){
+                int r = row + rows[i];
+                int c = col + cols[i];
+
+                if (r >= 0 && r < n && c >= 0 && c < n && !visited[r][c]){
+                    int newTime = max(t, grid[r][c]);
+                    if (newTime < time[r][c]) {
+                        time[r][c] = newTime;
+                        pq.push({newTime, {r, c}});
+                    }
+
+                }
+            }
         }
 
         return -1;
