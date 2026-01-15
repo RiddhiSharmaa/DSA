b27536c7dd6b00582040fbb7c44b77fc5d28a354
RiddhiSharmaa <Riddhisharma9900@gmail.com>
2026-01-15 18:54:12 +0530

    Time: 206 ms (68.55%) | Memory: 86.4 MB (70.12%) - LeetSync

--- 4129-number-of-centered-subarrays/number-of-centered-subarrays.cpp
+++ 4129-number-of-centered-subarrays/number-of-centered-subarrays.cpp
@@ -2,24 +2,39 @@ class Solution {
 public:
     int centeredSubarrays(vector<int>& nums) {
         int cnt = 0;
-        int sum = 0;
-        unordered_map<int, int>mpp;
 
         for (int i = 0; i < nums.size(); i++){
-            int sum = nums[i];
-            mpp[nums[i]]++;
-            if (mpp[sum] > 0) cnt++;
-            for (int j = i+1; j < nums.size(); j++){
+            int sum = 0;
+            unordered_set<int>st;
+            for (int j = i; j < nums.size(); j++){
+                st.insert(nums[j]);
                 sum += nums[j];
-                mpp[nums[j]]++;
-                if (mpp[sum] > 0) cnt++;
-            }
-            
-            for (auto& [num, freq] : mpp){
-                freq = 0;
+                if (st.count(sum) > 0) cnt++;
             }
         }
 
         return cnt;
     }
+    // int centeredSubarrays(vector<int>& nums) {
+    //     int cnt = 0;
+    //     int sum = 0;
+    //     unordered_map<int, int>mpp;
+
+    //     for (int i = 0; i < nums.size(); i++){
+    //         int sum = nums[i];
+    //         mpp[nums[i]]++;
+    //         if (mpp[sum] > 0) cnt++;
+    //         for (int j = i+1; j < nums.size(); j++){
+    //             sum += nums[j];
+    //             mpp[nums[j]]++;
+    //             if (mpp[sum] > 0) cnt++;
+    //         }
+            
+    //         for (auto& [num, freq] : mpp){
+    //             freq = 0;
+    //         }
+    //     }
+
+    //     return cnt;
+    // }
 };
