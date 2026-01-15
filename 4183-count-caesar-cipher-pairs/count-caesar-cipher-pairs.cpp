dd93a1045b7169d993f944f6d5e720ea85ca5950
RiddhiSharmaa <Riddhisharma9900@gmail.com>
2026-01-15 19:54:04 +0530

    Time: 53 ms (44.62%) | Memory: 118.7 MB (42.49%) - LeetSync

---
+++ 4183-count-caesar-cipher-pairs/count-caesar-cipher-pairs.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    long long countPairs(vector<string>& words) {
+        long long ans = 0;
+        unordered_map<string, long long>mpp;
+
+        for (int i = 0; i < words.size(); i++){
+            string s = words[i];
+            string key = "";
+            for (int j = 0; j < s.size(); j++){
+                int num = (s[j] - s[0] + 26) % 26;
+                char c = num + 'a';
+                key += c + '#';
+            }
+
+            mpp[key]++;
+        }
+
+        for (auto& [key, freq] : mpp){
+            long long n = freq;
+            n--;
+            ans += (n * (n+1))/2;
+        }
+
+        return ans;
+    }
+    // long long countPairs(vector<string>& words) {
+    //     long long ans = 0;
+    //     for (int i = 0; i < words.size(); i++){
+    //         string s = words[i];
+    //         for (int j = i+1; j < words.size(); j++){
+    //             string t = words[j];
+    //             if (s == t){
+    //                 ans++;
+    //                 continue;
+    //             }
+    //             int diff = (t[0] - s[0] + 26) % 26;
+    //             bool isSimilar = true;
+    //             for (int k = 0; k < s.size(); k++){
+    //                 int num = (t[k] - s[k] + 26) % 26;
+    //                 if (num != diff) {
+    //                     isSimilar = false;
+    //                     break;
+    //                 }
+    //             }
+
+    //             if (isSimilar) ans++;
+    //         }
+    //     }
+
+    //     return ans;
+    // }
+};
