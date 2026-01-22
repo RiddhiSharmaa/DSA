4e81f341c5165d15bc7bf8fd189c6ff38343f79a
RiddhiSharmaa <Riddhisharma9900@gmail.com>
2026-01-22 22:40:46 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (64.59%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,30 +1,53 @@
 class Solution {
 public:
-    bool isPalindrome(int i, int j, string& s){
-        int left = i, right = j;
-
-        while (left < right){
-            if (s[left] != s[right]){
-                return false;
-            }
-            left++;
-            right--;
+    void countPalindrome(int left, int right, int &cnt, string &s){
+        while (left >= 0 && right < s.size() && left <= right && s[left] == s[right]){
+            cnt++;
+            left--;
+            right++;
         }
-
-        return true;
     }
+
     int countSubstrings(string s) {
         int n = s.size();
         int cnt = 0;
 
         for (int i = 0; i < n; i++){
-            for (int j = i; j < n; j++){
-                if (s[i] == s[j] && isPalindrome(i, j, s)){
-                    cnt++;
-                }
-            }
+            int left = i, right = i;
+            countPalindrome(left, right, cnt, s);
+
+            left = i, right = i+1;
+            countPalindrome(left, right, cnt, s);
         }
 
         return cnt;
     }
+
+    // bool isPalindrome(int i, int j, string& s){
+    //     int left = i, right = j;
+
+    //     while (left < right){
+    //         if (s[left] != s[right]){
+    //             return false;
+    //         }
+    //         left++;
+    //         right--;
+    //     }
+
+    //     return true;
+    // }
+    // int countSubstrings(string s) {
+    //     int n = s.size();
+    //     int cnt = 0;
+
+    //     for (int i = 0; i < n; i++){
+    //         for (int j = i; j < n; j++){
+    //             if (s[i] == s[j] && isPalindrome(i, j, s)){
+    //                 cnt++;
+    //             }
+    //         }
+    //     }
+
+    //     return cnt;
+    // }
 };
