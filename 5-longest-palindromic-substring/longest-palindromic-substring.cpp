052052006b79365fc5f815d6f025f4682b4c364a
RiddhiSharmaa <Riddhisharma9900@gmail.com>
2026-01-22 14:23:12 +0530

    Time: 11 ms (63.85%) | Memory: 22.1 MB (36.36%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,33 +1,63 @@
 class Solution {
 public:
-    bool isPalindrome(int i, int j, string& s){
-        int left = i, right = j;
-
-        while (left <= right){
-            if (s[left] != s[right]){
-                return false;
-            }
-            left++;
-            right--;
-        }
-
-        return true;
-    }
-
     string longestPalindrome(string s) {
+        int start = INT_MIN, end = INT_MIN;
         string ans = "";
-        int n = s.size();
-        int left = 0, right = n-1;
-        int startIdx, endIdx;
 
-        for (int i = 0; i < n; i++){
-            for (int j = i; j < n; j++){
-                if (isPalindrome(i,j, s) && (j-i+1) > ans.length()){
-                    ans = s.substr(i, j-i+1);
+        for (int i = 0; i < s.size(); i++){
+            int left = i, right = i;
+            while (left >= 0 && right < s.size() && s[left] == s[right]){
+                if (right-left+1 > ans.size()){
+                    // start = left;
+                    // end = right;
+                    ans = s.substr(left, right-left+1);
                 }
+                left--;
+                right++;
+            }
+
+            left = i, right = i+1;
+            while (left >= 0 && right < s.size() && s[left] == s[right]){
+                if (right-left+1 > ans.size()){
+                    // start = left;
+                    // end = right;
+                    ans = s.substr(left, right-left+1);
+                }
+                left--;
+                right++;
             }
         }
 
         return ans;
     }
+    // bool isPalindrome(int i, int j, string& s){
+    //     int left = i, right = j;
+
+    //     while (left <= right){
+    //         if (s[left] != s[right]){
+    //             return false;
+    //         }
+    //         left++;
+    //         right--;
+    //     }
+
+    //     return true;
+    // }
+
+    // string longestPalindrome(string s) {
+    //     string ans = "";
+    //     int n = s.size();
+    //     int left = 0, right = n-1;
+    //     int startIdx, endIdx;
+
+    //     for (int i = 0; i < n; i++){
+    //         for (int j = i; j < n; j++){
+    //             if (isPalindrome(i,j, s) && (j-i+1) > ans.length()){
+    //                 ans = s.substr(i, j-i+1);
+    //             }
+    //         }
+    //     }
+
+    //     return ans;
+    // }
 };
