5d97c52d9edfc48b52fae4274aa413afb5808d6b
RiddhiSharmaa <Riddhisharma9900@gmail.com>
2026-01-22 22:13:29 +0530

    Time: 391 ms (13.07%) | Memory: 292 MB (7.63%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,28 +1,30 @@
 class Solution {
 public:
+    int start = 0, end = 0, maxLen = INT_MIN;
+    bool isPalindrome(int i, int j, vector<vector<int>>& dp, string &s){
+        if (i >= j) return true;
 
-    string longestPalindrome(string s) {
-        int start = 0, end = 0;
+        if (dp[i][j] != -1) return dp[i][j];
 
-        for (int i = 0; i < s.size(); i++){
-            int left = i, right = i;
-            while (left >= 0 && right < s.size() && s[left] == s[right]){
-                if (right-left+1 > end-start+1){
-                    start = left;
-                    end = right;
-                }
-                left--;
-                right++;
+        if (s[i] == s[j] && isPalindrome(i+1, j-1, dp, s)){
+            if ((j-i+1) > (end-start+1)){
+                start = i;
+                end = j;
             }
 
-            left = i, right = i+1;
-            while (left >= 0 && right < s.size() && s[left] == s[right]){
-                if (right-left+1 > end-start+1){
-                    start = left;
-                    end = right;
-                }
-                left--;
-                right++;
+            return dp[i][j] = true;
+        }
+
+        return dp[i][j] = false;
+    }
+
+    string longestPalindrome(string s){
+        int n = s.size();
+        vector<vector<int>>dp(n, vector<int>(n, -1));
+
+        for (int i = 0; i < n; i++){
+            for (int j = i; j < n; j++){
+                isPalindrome(i, j, dp, s);
             }
         }
 
@@ -30,6 +32,37 @@ public:
         return ans;
     }
 
+    // O(n^2)
+    // string longestPalindrome(string s) {
+    //     int start = 0, end = 0;
+
+    //     for (int i = 0; i < s.size(); i++){
+    //         int left = i, right = i;
+    //         while (left >= 0 && right < s.size() && s[left] == s[right]){
+    //             if (right-left+1 > end-start+1){
+    //                 start = left;
+    //                 end = right;
+    //             }
+    //             left--;
+    //             right++;
+    //         }
+
+    //         left = i, right = i+1;
+    //         while (left >= 0 && right < s.size() && s[left] == s[right]){
+    //             if (right-left+1 > end-start+1){
+    //                 start = left;
+    //                 end = right;
+    //             }
+    //             left--;
+    //             right++;
+    //         }
+    //     }
+
+    //     string ans = s.substr(start, end-start+1);
+    //     return ans;
+    // }
+
+    // O(n^3)
     // bool isPalindrome(int i, int j, string& s){
     //     int left = i, right = j;
 
