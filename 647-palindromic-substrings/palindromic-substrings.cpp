31a9a7caa0b970d7936388350fb992ce8930d494
RiddhiSharmaa <Riddhisharma9900@gmail.com>
2026-01-23 10:18:51 +0530

    Time: 527 ms (13.54%) | Memory: 34.7 MB (14.54%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,27 +1,50 @@
 class Solution {
 public:
-    void countPalindrome(int left, int right, int &cnt, string &s){
-        while (left >= 0 && right < s.size() && left <= right && s[left] == s[right]){
-            cnt++;
-            left--;
-            right++;
+    bool checkPalindrome(int i, int j, string &s){
+        if (i > j) {
+            return true;
         }
+        // if (dp[i][j] != -1) return 
+        if (s[i] == s[j]){
+            return checkPalindrome(i+1, j-1, s);
+        }
+        return false;
     }
-
     int countSubstrings(string s) {
         int n = s.size();
         int cnt = 0;
+        vector<vector<int>>dp(n, vector<int>(n, -1));
 
         for (int i = 0; i < n; i++){
-            int left = i, right = i;
-            countPalindrome(left, right, cnt, s);
-
-            left = i, right = i+1;
-            countPalindrome(left, right, cnt, s);
+            for (int j = i; j < n; j++){
+                if (checkPalindrome(i, j, s)) cnt++;
+            }
         }
 
         return cnt;
     }
+    // void countPalindrome(int left, int right, int &cnt, string &s){
+    //     while (left >= 0 && right < s.size() && left <= right && s[left] == s[right]){
+    //         cnt++;
+    //         left--;
+    //         right++;
+    //     }
+    // }
+
+    // int countSubstrings(string s) {
+    //     int n = s.size();
+    //     int cnt = 0;
+
+    //     for (int i = 0; i < n; i++){
+    //         int left = i, right = i;
+    //         countPalindrome(left, right, cnt, s);
+
+    //         left = i, right = i+1;
+    //         countPalindrome(left, right, cnt, s);
+    //     }
+
+    //     return cnt;
+    // }
 
     // bool isPalindrome(int i, int j, string& s){
     //     int left = i, right = j;
