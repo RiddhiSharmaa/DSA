70608baf17c98df73a91ea91b8ee6e1420201f79
RiddhiSharmaa <Riddhisharma9900@gmail.com>
2026-01-25 13:38:16 +0530

    Time: 526 ms (47.04%) | Memory: 435.6 MB (44.30%) - LeetSync

---
+++ 4180-pythagorean-distance-nodes-in-a-tree/pythagorean-distance-nodes-in-a-tree.cpp
@@ -0,0 +1,67 @@
+class Solution {
+public:
+    using ll = long long;
+    bool pythagoras(vector<int>& s){
+        ll a = pow(s[0], 2);
+        ll b = pow(s[1], 2);
+        ll c = pow(s[2], 2);
+        // cout << a << " " << b<<  " " <<c<< " ";
+        // cout << endl;
+
+        if (a + b == c) return true;
+        return false;
+    }
+
+    void bfs(int src, vector<vector<int>>& adj, vector<int>& dist){
+        dist[src] = 0;
+        
+        queue<pair<int, int>>q;
+        q.push({src, 0});
+
+        while (!q.empty()){
+            int node = q.front().first;
+            int dis = q.front().second;
+            q.pop();
+
+            for (auto& neigh : adj[node]){
+                int adjNode = neigh;
+                if (dist[adjNode] > dis + 1){
+                    dist[adjNode] = dis+1;
+                    q.push({adjNode, dis+1});
+                }
+            }
+        }
+
+    }
+    int specialNodes(int n, vector<vector<int>>& edges, int x, int y, int z) {
+        vector<vector<int>>adj(n);
+        vector<int>distX(n, 1e9), distY(n, 1e9), distZ(n, 1e9);
+
+        for (auto& e : edges){
+            int u = e[0];
+            int v = e[1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+
+        bfs(x, adj, distX);
+        bfs(y, adj, distY);
+        bfs(z, adj, distZ);
+        
+        int cnt = 0;
+        for (int i = 0; i < n; i++){
+            ll dx = distX[i];
+            ll dy = distY[i];
+            ll dz = distZ[i];
+            
+            if (dx >= 1e9 || dy >= 1e9 || dz >= 1e9) continue;
+            vector<ll> s = {dx, dy, dz};
+            
+            sort(s.begin(), s.end());
+            
+            if ((s[0]*s[0]) + (s[1]*s[1]) == (s[2]*s[2])) cnt++;
+        }
+
+        return cnt;
+    }
+};
