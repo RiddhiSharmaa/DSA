500852845f8fcad8f4fe3feb106cf647f219f918
RiddhiSharmaa <Riddhisharma9900@gmail.com>
2026-02-01 10:39:48 +0530

    Time: 194 ms (11.11%) | Memory: 187.5 MB (5.56%) - LeetSync

--- 4176-minimum-k-to-reduce-array-within-limit/minimum-k-to-reduce-array-within-limit.cpp
+++ 4176-minimum-k-to-reduce-array-within-limit/minimum-k-to-reduce-array-within-limit.cpp
@@ -1,15 +1,6 @@
 class Solution {
 public:
     using ll = long long;
-    int nonPositive(int n, vector<int>& nums){
-        ll cnt = 0;
-        for (int i = 0; i< nums.size(); i++){
-            ll k = nums[i];
-            cnt += (nums[i] + n -1)/ n;
-        }
-
-        return (int)cnt;
-    }
     int minimumK(vector<int>& nums) {
         sort(nums.begin(), nums.end());
         int ans = -1;
@@ -17,17 +8,20 @@ public:
         
         while (low <= high){
             ll mid = low + (high - low)/2;
-            ll a = nonPositive(mid, nums);
+            ll oper = 0;
 
-            if (a <= mid * mid){
+            for (int num : nums){
+                oper += (num + mid - 1) / mid;
+            }
+
+            if (oper <= mid * mid){
                 high = mid-1;
-                
                 ans = (int)mid;
             } else {
                 low = mid+1;
             }
-        
         }
+
         return ans;
     }  
 };
