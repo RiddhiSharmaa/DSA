a9d5c2a37eab73dda8d2f13290a1034bc98718b8
RiddhiSharmaa <Riddhisharma9900@gmail.com>
2026-02-09 00:47:14 +0530

    Time: 0 ms (100.00%) | Memory: 21.9 MB (67.14%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.cpp
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -6,30 +6,16 @@ public:
         vector<int>ans;
 
         for (int i = 0; i < n; i++){
-            bool destroyed = true;
-            
-            while (!st.empty() && st.top() > 0 && asteroids[i] < 0){
-                if (abs(asteroids[i]) > st.top()){
-                    st.pop();
-                } else if (abs(asteroids[i]) == st.top()){
-                    st.pop();
-                    destroyed = false;
-                    break;
-                } else {
-                    destroyed = false;
-                    break;
-                } 
+            ans.push_back(asteroids[i]);
+            while (ans.size() >= 2 && ans.back() < 0 && ans[ans.size()-2] > 0){
+                if (abs(ans.back()) > ans[ans.size()-2]){
+                    ans[ans.size()-2] = ans.back();
+                } else if (abs(ans.back()) == ans[ans.size()-2]){
+                    ans.pop_back();
+                }
+                ans.pop_back();
             }
-            
-            if (destroyed) st.push(asteroids[i]);
         }
-
-        while (!st.empty()){
-            ans.push_back(st.top());
-            st.pop();
-        }
-
-        reverse(ans.begin(), ans.end());
 
         return ans;
     }
