9a53b157b327d1811da6d7f824865e0c2ce2db91
RiddhiSharmaa <Riddhisharma9900@gmail.com>
2026-02-08 16:22:19 +0530

    Time: 109 ms (37.90%) | Memory: 191.6 MB (48.90%) - LeetSync

--- 4211-count-subarrays-with-cost-less-than-or-equal-to-k/count-subarrays-with-cost-less-than-or-equal-to-k.cpp
+++ 4211-count-subarrays-with-cost-less-than-or-equal-to-k/count-subarrays-with-cost-less-than-or-equal-to-k.cpp
@@ -2,25 +2,34 @@ class Solution {
 public:
     long long countSubarrays(vector<int>& nums, long long k) {
         long long left = 0, cost = 0, cnt = 0;
-        map<long long, long long>mpp;
+        deque<long long>maxDq, minDq;
 
-        for (long long right = 0; right < nums.size(); right++){
-            mpp[nums[right]]++;
+        for (int right = 0; right < nums.size(); right++){
+            while (!maxDq.empty() && nums[right] >= nums[maxDq.back()]){
+                maxDq.pop_back();
+            }
+            maxDq.push_back(right);
 
-            cost = (mpp.rbegin()->first - mpp.begin()->first)*(right-left+1);
+            while (!minDq.empty() && nums[right] <= nums[minDq.back()]){
+                minDq.pop_back();
+            }
+            minDq.push_back(right);
 
-            while (cost > k){
-                mpp[nums[left]]--;
-                if (mpp[nums[left]] == 0) {
-                    mpp.erase(nums[left]);
-                } 
+            while (!maxDq.empty() && !minDq.empty()){
+                long long maxVal = nums[maxDq.front()];
+                long long minVal = nums[minDq.front()];
+                long long len = right - left + 1;
+
+                cost = (maxVal - minVal) * len;
+
+                if (cost <= k) break;
+                if (maxDq.front() == left) maxDq.pop_front();
+                if (minDq.front() == left) minDq.pop_front();
+
                 left++;
-                cost = (mpp.rbegin()->first - mpp.begin()->first)*(right-left+1);
             }
-            
-            if (cost <= k) {
-                cnt += (right-left+1);
-            }
+
+            cnt += (right-left+1);
         }
 
         return cnt;
