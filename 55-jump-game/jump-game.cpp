c7f159f48940e450295f77b032ccd68835abc8fd
RiddhiSharmaa <Riddhisharma9900@gmail.com>
2026-02-14 01:08:41 +0530

    Time: 666 ms (5.94%) | Memory: 54.9 MB (8.35%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,22 +1,36 @@
 class Solution {
 public:
-    bool helper(int i, vector<int>& nums, vector<int>& dp){
-        if (i == nums.size()-1) return  true;
-        if (i > nums.size() ) return false;
-
-        if (dp[i] != -1) return dp[i];
-        for (int j = i+1; j <= i + nums[i] && j < nums.size(); j++){
-            if (helper(j, nums, dp)){
-                return dp[i] = true;
-            }
-        }
-
-        return dp[i] = false;
-    }
     bool canJump(vector<int>& nums) {
         int n = nums.size();
-        vector<int>dp(n, -1);
+        vector<int>dp(n+1, 0);
 
-        return helper(0, nums, dp);
+        dp[0] = 1;
+        for (int i = 0; i < n; i++){
+            for (int j = i+1; j <= i + nums[i] && j < n; j++){
+                if (dp[i]) dp[j] = 1;
+            }
+        }
+        
+        return dp[n-1];
     }
+
+    // bool helper(int i, vector<int>& nums, vector<int>& dp){
+    //     if (i == nums.size()-1) return  true;
+    //     if (i > nums.size() ) return false;
+
+    //     if (dp[i] != -1) return dp[i];
+    //     for (int j = i+1; j <= i + nums[i] && j < nums.size(); j++){
+    //         if (helper(j, nums, dp)){
+    //             return dp[i] = true;
+    //         }
+    //     }
+
+    //     return dp[i] = false;
+    // }
+    // bool canJump(vector<int>& nums) {
+    //     int n = nums.size();
+    //     vector<int>dp(n, -1);
+
+    //     return helper(0, nums, dp);
+    // }
 };
