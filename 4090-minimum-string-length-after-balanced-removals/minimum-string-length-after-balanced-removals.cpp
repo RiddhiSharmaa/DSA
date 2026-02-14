9fc8889dfb467e05978810b9067213fbc5404989
RiddhiSharmaa <Riddhisharma9900@gmail.com>
2026-02-14 19:30:59 +0530

    Time: 13 ms (7.92%) | Memory: 19.3 MB (5.06%) - LeetSync

--- 4090-minimum-string-length-after-balanced-removals/minimum-string-length-after-balanced-removals.cpp
+++ 4090-minimum-string-length-after-balanced-removals/minimum-string-length-after-balanced-removals.cpp
@@ -7,9 +7,9 @@ public:
         for (int i = 0; i < s.size(); i++){
             if (!st.empty() && st.top() != s[i]){
                 st.pop();
-                continue;
+            } else{
+                st.push(s[i]);
             }
-            st.push(s[i]);
         }
 
         return st.size();
