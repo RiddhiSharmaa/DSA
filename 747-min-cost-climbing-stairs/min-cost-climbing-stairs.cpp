abe2d5bc3fb21199983903e1660bcc44af180ba0
RiddhiSharmaa <Riddhisharma9900@gmail.com>
2026-02-16 18:28:04 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (52.29%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
@@ -1,30 +1,22 @@
 class Solution {
 public:
-    int minCostClimbingStairs(vector<int>& cost) {
-        int n = cost.size();
-        vector<int>dp(n+1, -1);
-        dp[0] = 0;
-        dp[1] = 0;
+    int helper(int i, vector<int>& cost, vector<int>& dp){
+        if (i >= cost.size()) return 0;
+        if (dp[i] != -1) return dp[i];
 
-        for (int i = 2; i <= n; i++){
-            dp[i] = min(cost[i-1] + dp[i-1], cost[i-2] + dp[i-2]);
-        }
+        int pick = 0, notPick = 0;
+        pick = cost[i] + helper(i+1, cost, dp);
+        notPick = cost[i] + helper(i+2, cost, dp);
+        // notPick = helper(i+1, cost, dp);
 
-        return dp[n];
+        return dp[i] = min(pick, notPick);
     }
-    // int helper(int i, vector<int>& dp, vector<int>& cost){
-    //     if (i >= dp.size()-1) return 0;
-    //     if (dp[i] != -1) return dp[i];
-        
-    //     return dp[i] = cost[i] + min(helper(i+1, dp, cost), helper(i+2, dp, cost));
-    // }
-    // int minCostClimbingStairs(vector<int>& cost) {
-    //     int n = cost.size();
-    //     vector<int>dp(n+1, -1);
 
-    //     dp[n] = 0;
-    //     helper(0, dp, cost);
+    int minCostClimbingStairs(vector<int>& cost) {
+        int minCost = 0;
+        int n = cost.size();
+        vector<int>dp(n, -1);
 
-    //     return min(dp[0], dp[1]);
-    // }
+        return min(helper(0, cost, dp), helper(1, cost, dp));
+    }
 };
