4b5222af5ae4b4fe3fa55c6e96e093578b63a1d3
RiddhiSharmaa <Riddhisharma9900@gmail.com>
2026-03-02 16:18:42 +0530

    Time: 8 ms (6.47%) | Memory: 80.5 MB (73.10%) - LeetSync

--- 2519-find-the-original-array-of-prefix-xor/find-the-original-array-of-prefix-xor.cpp
+++ 2519-find-the-original-array-of-prefix-xor/find-the-original-array-of-prefix-xor.cpp
@@ -5,10 +5,8 @@ public:
         vector<int>ans(n, 0);
 
         ans[0] = pref[0];
-        int xorSoFar = ans[0];
         for (int i = 1; i < n; i++){
-            ans[i] = xorSoFar ^ pref[i];
-            xorSoFar ^= ans[i];
+            ans[i] = pref[i] ^ pref[i-1];
         }
 
         return ans;
