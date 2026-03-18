a7c1916d1fdb50e8e712392b495862e6449b1343
RiddhiSharmaa <Riddhisharma9900@gmail.com>
2026-03-18 18:42:47 +0530

    Time: 43 ms (36.82%) | Memory: 32.1 MB (75.01%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.cpp
+++ 876-hand-of-straights/hand-of-straights.cpp
@@ -4,30 +4,28 @@ public:
         int n = hand.size();
         if (n % groupSize != 0) return false;
 
-        // sort(hand.begin(), hand.end());
+        map<int, int>mpp;
 
-        // for (int i = 0; i < n; i += groupSize){
-        //     for (int j = i; j < i + groupSize - 1; j++){
-        //         if (hand[j+1] == hand[j] || hand[j+1] > hand[j] + 1) return false;
-        //     }
-        // }
-
-        multiset<int>ms(hand.begin(), hand.end());
+        for (int i = 0; i < n; i++){
+            mpp[hand[i]]++;
+        }
 
         int groups = n / groupSize;
 
-        for (int i = 0; i < groups; i++){
-            auto it = ms.begin();
-            int num = *it;
-            ms.erase(it);
-            for (int j = 0; j < groupSize-1; j++){
-                auto it2 = ms.find(num+1);
-                if (it2 != ms.end()){
-                    ms.erase(it2);
-                } else {
-                    return false;
+        for (auto [num, freq] : mpp){
+            int count = freq;
+            while (count){
+                if (mpp[num] == 0) continue;
+                int k = num;
+                for (int j = 0; j < groupSize; j++){
+                    if (mpp[k] > 0){
+                        mpp[k]--;
+                    } else {
+                        return false;
+                    }
+                    k++;
                 }
-                num++;
+                count--;
             }
         }
 
