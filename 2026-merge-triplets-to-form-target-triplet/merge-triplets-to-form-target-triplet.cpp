0e729ef983ee406fc67499ee229eb64aaad8b0cd
RiddhiSharmaa <Riddhisharma9900@gmail.com>
2026-03-19 01:26:11 +0530

    Time: 3 ms (61.32%) | Memory: 150.4 MB (88.10%) - LeetSync

---
+++ 2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool mergeTriplets(vector<vector<int>>& triplets, vector<int>& target) {
+        int n = triplets.size(), m = triplets[0].size();
+        bool a = false, b = false, c = false;
+
+        for (int i = 0; i < n; i++){
+            if (triplets[i][0] > target[0] || triplets[i][1] > target[1] || 
+            triplets[i][2] > target[2]) continue;
+
+            if (triplets[i][0] == target[0]) a = true;
+            if (triplets[i][1] == target[1]) b = true;
+            if (triplets[i][2] == target[2]) c = true;
+        }
+
+        return (a == b && b == c && a == true) ? true : false;
+    }
+};
