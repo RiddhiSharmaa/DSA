f9bce6a85994993e1233842798aeeda187096fc3
RiddhiSharmaa <Riddhisharma9900@gmail.com>
2026-03-22 11:37:45 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (70.61%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,22 +1,12 @@
 class Solution {
 public:
-    double helperEven(double x, int n){
+    double helperEven(double x, long long n){
         if (n == 1) return x;
         if (x == 0) return 0;
-        if (n < 0) return helperEven(1/x, -n);
         if (n == 0) return 1;
+
         double ans = 1;
-        if (n%2 == 0){
-            ans = helperEven(x, n/2);
-            ans *= ans;
-        } else {
-            ans = helperEven(x, n-1);
-            ans = x * ans;
-        }
-        return ans;
-    }
-    double myPow(double x, int n) {
-        double ans = 1;
+        
         if (n%2 == 0){
             ans = helperEven(x, n/2);
             ans *= ans;
@@ -26,5 +16,15 @@ public:
         }
 
         return ans;
+    }
+    double myPow(double x, int n) {
+        double ans = 1;
+        if (n < 0) {
+            long long N = -1LL * n;
+            x = 1/x;
+            return helperEven(x, N);
+        }
+
+        return helperEven(x, n);
     }
 };
