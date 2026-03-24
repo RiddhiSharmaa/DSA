e89a2ba8c24116dfb07a7cf963dc8f70955f67e0
RiddhiSharmaa <Riddhisharma9900@gmail.com>
2026-03-24 14:28:25 +0530

    Time: 0 ms (100.00%) | Memory: 55.1 MB (62.01%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cpp
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -2,18 +2,18 @@ class Solution {
 public:
     using ll = long long;
     int firstMissingPositive(vector<int>& nums) {
-        unordered_set<int>st;
-        ll maxi = INT_MIN;
+        int n = nums.size();
 
-        for (int i = 0; i < nums.size(); i++) {
-            st.insert(nums[i]);
-            maxi = max(maxi, (ll)nums[i]);
+        for (int i = 0; i < n; i++){
+            while (nums[i] >= 1 && nums[i] <= n && nums[i] != nums[nums[i]-1]){
+                swap(nums[i], nums[nums[i]-1]);
+            }
         }
 
-        for (ll i = 1; i <= maxi + 1; i++){
-            if (st.find(i) == st.end()) return (int)i;
+        for (int i = 0 ; i < n; i++){
+            if (nums[i] != i+1) return i+1;
         }
 
-        return 1;
+        return n+1;
     }
 };
