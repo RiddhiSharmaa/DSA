874628655bddf3af01449d75f7441c26e2341bb6
RiddhiSharmaa <Riddhisharma9900@gmail.com>
2026-03-24 16:46:43 +0530

    Time: 3 ms (73.74%) | Memory: 48.4 MB (82.16%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.cpp
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.cpp
@@ -2,16 +2,18 @@ class Solution {
 public:
     vector<int> findDuplicates(vector<int>& nums) {
         int n = nums.size();
-        unordered_set<int>st;
+        vector<int>ans;
 
         for (int i = 0; i < n; i++){
             while (nums[i] >= 1 && nums[i] <= n && nums[i] != nums[nums[i]-1]){
                 swap(nums[i], nums[nums[i]-1]);
             }
-            if (nums[i] == nums[nums[i]-1] && i != nums[i]-1) st.insert(nums[i]);
+            if (nums[i] >= 1 && nums[i] == nums[nums[i]-1] && i != nums[i]-1) {
+                ans.push_back(nums[i]);
+                nums[i] = -1;    
+            }
         }
 
-        vector<int>ans(st.begin(), st.end());
         return ans;
     }
 };
