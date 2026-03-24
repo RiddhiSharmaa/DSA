379f63d9e02f63e0546ce8be29a4f5b453cd05b4
RiddhiSharmaa <Riddhisharma9900@gmail.com>
2026-03-24 18:20:13 +0530

    Time: 0 ms (100.00%) | Memory: 25.1 MB (88.71%) - LeetSync

---
+++ 645-set-mismatch/set-mismatch.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>ans;
+
+        for (int i = 0; i < n; i++){
+            while (nums[i] != nums[nums[i]-1]){
+                swap(nums[i], nums[nums[i]-1]);
+            }
+        }
+
+        for (int i = 0; i < n; i++){
+            if (nums[i] != i+1){
+                return {nums[i], i+1};
+            }
+        }
+
+        return {};
+    }
+};
