a17b06b0a1be2ff864c1bf64f0931b84fd61d41a
RiddhiSharmaa <Riddhisharma9900@gmail.com>
2026-03-27 19:42:38 +0530

    Time: 3 ms (34.75%) | Memory: 77.1 MB (44.88%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -11,30 +11,26 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* t1 = l1;
-        ListNode* t2 = l2;
-        int carry = 0;
         ListNode* prev = new ListNode();
         ListNode* dummy = prev;
+        int carry = 0;
 
-        while (t1 != nullptr || t2 != nullptr){
-            int n1 = t1 ? t1 -> val : 0;
-            int n2 = t2 ? t2 -> val : 0;
+        while (l1 != nullptr || l2 != nullptr){
+            int n1 = l1 ? l1 -> val : 0;
+            int n2 = l2 ? l2 -> val : 0;
             int n = n1 + n2 + carry;
             carry = n/10;
             int digit = n%10;
-            ListNode* newNode = new ListNode(digit);
-            prev -> next = newNode;
-            prev = newNode;
-            if (t1) t1 = t1 -> next;
-            if (t2) t2 = t2 -> next;
+            prev -> next = new ListNode(digit);
+            prev = prev -> next;
+            if (l1) l1 = l1 -> next;
+            if (l2) l2 = l2 -> next;
         }
 
         while (carry){
             int digit = carry%10;
-            ListNode* newNode = new ListNode(digit);
-            prev -> next = newNode;
-            prev = newNode;
+            prev -> next = new ListNode(digit);
+            prev = prev -> next;
             carry /= 10;
         }
 
