db68f58075947e5fdd8fa2a425893dcdb683c038
RiddhiSharmaa <Riddhisharma9900@gmail.com>
2026-03-28 16:04:32 +0530

    Time: 24 ms (96.97%) | Memory: 53.3 MB (92.60%) - LeetSync

---
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool checkPalindrome(string& k){
+        int n = k.size();
+
+        for (int i = 0; i < n/2; i++){
+            if (k[i] != k[n-i-1]) return false;
+        }
+
+        return true;
+    }
+    void helper(int i, vector<string>& temp, vector<vector<string>>& ans, string& s){
+        if (i >= s.size()){
+            ans.push_back(temp);
+            return;
+        }
+        for (int l = i; l < s.size(); l++){
+            string str = s.substr(i, l-i+1);
+            if (checkPalindrome(str)){
+                temp.push_back(str);
+                helper(l+1, temp, ans, s);
+                temp.pop_back();
+            }
+        }
+    }
+    vector<vector<string>> partition(string s) {
+        int n = s.size();
+        vector<vector<string>>ans;
+        vector<string>temp;
+
+        helper(0, temp, ans, s);
+
+        return ans;
+    }
+};
