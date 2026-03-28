4ea0cd5940ba34a42c89b75895d1133a39d04421
RiddhiSharmaa <Riddhisharma9900@gmail.com>
2026-03-28 20:37:55 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (58.59%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     void helper(int i, string s, string& digits, vector<string>& ans, 
-    unordered_map<char, string>& mpp){
+    vector<string>& mpp){
         if (s.size() == digits.size()){
             ans.push_back(s);
             return;
         }
 
-        for (char c : mpp[digits[i]]){
+        for (char c : mpp[digits[i]-'0']){
             s += c;
             helper(i+1, s, digits, ans, mpp);
             s.pop_back();
         }
     }
     vector<string> letterCombinations(string digits) {
-       unordered_map<char, string> mpp = {{'2', "abc"},{'3', "def"},{'4', "ghi"},
-       {'5', "jkl"},{'6', "mno"},{'7', "pqrs"},{'8', "tuv"},{'9', "wxyz"}};
+       vector<string> mpp = {"", "", "abc", "def" , "ghi", "jkl", "mno", "pqrs", "tuv",
+       "wxyz"};
 
         vector<string>ans;
         helper(0, "", digits, ans, mpp);
