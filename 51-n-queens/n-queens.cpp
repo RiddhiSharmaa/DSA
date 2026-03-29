aaf823df8c24a32568981dd5fab5eb528a3c8823
RiddhiSharmaa <Riddhisharma9900@gmail.com>
2026-03-29 15:16:25 +0530

    Time: 4 ms (31.85%) | Memory: 10.2 MB (55.29%) - LeetSync

---
+++ 51-n-queens/n-queens.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    bool isSafe(int i, int j, vector<string>& ans){
+        int n = ans.size();
+        for (int r = 0; r < n; r++){
+            if (ans[r][j] == 'Q') return false;
+        }
+
+        for (int c = 0; c < n; c++){
+            if (ans[i][c] == 'Q') return false;
+        }
+
+        for (int r = i, c = j; r >= 0 && c >= 0; r--, c--){
+            if (ans[r][c] == 'Q') return false;
+        }
+
+        for (int r = i, c = j; r >= 0 && c < n; r--, c++){
+            if (ans[r][c] == 'Q') return false;
+        }
+
+        return true;
+    }
+    void helper(int i, vector<string>& ans, vector<vector<string>>& result){
+        if (i == ans.size()) {
+            result.push_back(ans);
+            return;
+        }
+        if (i > ans.size()) return;
+
+        for (int j = 0; j < ans.size(); j++){
+            if (isSafe(i, j, ans)){
+                ans[i][j] = 'Q';
+                helper(i+1, ans, result);
+                ans[i][j] = '.';
+            }
+        }
+    }
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>>result;
+        vector<string>ans(n, string(n, '.'));
+        helper(0, ans, result);
+
+        return result;
+    }
+};
