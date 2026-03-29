5b4a7453bca47770cd02e732f732b0f851829963
RiddhiSharmaa <Riddhisharma9900@gmail.com>
2026-03-29 22:36:21 +0530

    Time: 0 ms (100.00%) | Memory: 15 MB (24.94%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
@@ -10,47 +10,29 @@
  */
 class Solution {
 public:
-    ListNode* reverseList(ListNode* head){
-        if (!head) return nullptr;
-        ListNode* curr = head;
-        ListNode* prev = nullptr;
-
-        while (curr != nullptr){
-            ListNode* nextNode = curr -> next;
-            curr -> next = prev;
-            prev = curr;
-            curr = nextNode;
-        }
-
-        return prev;
-    }
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        ListNode* temp = reverseList(head);
-        if (n == 1){
-            ListNode* toDelete = temp;
-            temp = temp -> next;
-            toDelete -> next = nullptr;
-            delete(toDelete);
-            ListNode* ans = reverseList(temp);
-            return ans;
-        }
-        int i = 0;
-        ListNode* dummy = temp;
-        ListNode* prev = temp;
+        ListNode* dummy = new ListNode();
+        dummy -> next = head;
 
-        while (i < n-1){
-            prev = temp;
-            temp = temp -> next;
+        ListNode* fast = dummy;
+        ListNode* slow = dummy;
+        
+        int i = 0;
+        while (i < n){
+            fast = fast -> next;
             i++;
         }
 
-        prev -> next = temp -> next;
-        temp -> next = nullptr;
+        while (fast -> next != nullptr){
+            fast = fast -> next;
+            slow = slow -> next;
+        }
 
-        delete(temp);
-        if (!dummy) return nullptr;
-        ListNode* t = reverseList(dummy);
-        
-        return t;
+        ListNode* toDelete = slow -> next;
+        slow -> next = toDelete -> next;
+        toDelete -> next = nullptr;
+        delete(toDelete);
+
+        return dummy -> next;
     }
 };
