d453b1735a69f2c6f65613fd20f4587573c6af89
RiddhiSharmaa <Riddhisharma9900@gmail.com>
2026-04-07 10:07:53 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (73.33%) - LeetSync

--- 316-remove-duplicate-letters/remove-duplicate-letters.cpp
+++ 316-remove-duplicate-letters/remove-duplicate-letters.cpp
@@ -4,7 +4,6 @@ public:
         int n = s.size();
         vector<int>visited(26, 0);
         unordered_map<char, int>freq;
-        stack<char>st;
         string ans = "";
 
         for (char c : s){
@@ -12,23 +11,17 @@ public:
         }
 
         for (int i = 0; i < n; i++){
-            while (!st.empty() && s[i] <= st.top() && freq[st.top()] >= 1 
+            while (!ans.empty() && s[i] <= ans.back() && freq[ans.back()] >= 1 
             && !visited[s[i]-'a']){
-                visited[st.top() - 'a'] = 0;
-                st.pop();
+                visited[ans.back() - 'a'] = 0;
+                ans.pop_back();
             }
 
-            if (!visited[s[i] - 'a']) st.push(s[i]);
+            if (!visited[s[i] - 'a']) ans.push_back(s[i]);
             visited[s[i] - 'a'] = 1;
             freq[s[i]]--;
         }
 
-        while (!st.empty()){
-            ans += st.top();
-            st.pop();
-        }
-
-        reverse(ans.begin(), ans.end());
         return ans;
     }
 };
