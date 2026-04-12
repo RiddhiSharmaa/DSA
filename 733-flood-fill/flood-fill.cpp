b6acd5b1fbf8130f8c32f4a88e3894aeb899008f
RiddhiSharmaa <Riddhisharma9900@gmail.com>
2026-04-13 00:01:34 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (74.37%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,9 +1,7 @@
 class Solution {
 public:
     void helper(int sr, int sc, int& originalColor, int &color, vector<int>row, 
-    vector<int>& col, vector<vector<int>>& image, vector<vector<int>>& visited){
-        if (visited[sr][sc]) return;
-        visited[sr][sc] = 1;
+    vector<int>& col, vector<vector<int>>& image){
         image[sr][sc] = color;
 
         for (int i = 0; i < row.size(); i++){
@@ -11,20 +9,20 @@ public:
             int c = col[i] + sc;
 
             if (r >= 0 && r < image.size() && c >= 0 && c < image[0].size() &&
-            !visited[r][c] && image[r][c] == originalColor){
-                helper(r, c, originalColor, color, row, col, image, visited);
+            image[r][c] == originalColor){
+                helper(r, c, originalColor, color, row, col, image);
             }
         }
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        if (image[sr][sc] == color) return image;
         int n = image.size();
         int m = image[0].size();
-        vector<vector<int>>visited(n, vector<int>(m, 0));
         vector<int>row = {-1, 0, 1, 0};
         vector<int>col = {0, 1, 0, -1};
         int originalColor = image[sr][sc];
 
-        helper(sr, sc, originalColor, color, row, col, image, visited);
+        helper(sr, sc, originalColor, color, row, col, image);
         return image;
     }
 };
