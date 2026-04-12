930847b76890e93564de256a15fbec24f2ecd659
RiddhiSharmaa <Riddhisharma9900@gmail.com>
2026-04-13 00:05:24 +0530

    Time: 0 ms (100.00%) | Memory: 18.4 MB (31.18%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,19 +1,5 @@
 class Solution {
 public:
-    void helper(int sr, int sc, int& originalColor, int &color, vector<int>row, 
-    vector<int>& col, vector<vector<int>>& image){
-        image[sr][sc] = color;
-
-        for (int i = 0; i < row.size(); i++){
-            int r = row[i] + sr;
-            int c = col[i] + sc;
-
-            if (r >= 0 && r < image.size() && c >= 0 && c < image[0].size() &&
-            image[r][c] == originalColor){
-                helper(r, c, originalColor, color, row, col, image);
-            }
-        }
-    }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         if (image[sr][sc] == color) return image;
         int n = image.size();
@@ -21,8 +7,27 @@ public:
         vector<int>row = {-1, 0, 1, 0};
         vector<int>col = {0, 1, 0, -1};
         int originalColor = image[sr][sc];
+        queue<pair<int, int>>q;
+        q.push({sr, sc});
 
-        helper(sr, sc, originalColor, color, row, col, image);
+        while (!q.empty()){
+            auto p = q.front();
+            q.pop();
+            int nrow = p.first;
+            int ncol = p.second;
+            image[nrow][ncol] = color;
+
+            for (int i = 0; i < row.size(); i++){
+                int r = row[i] + nrow;
+                int c = col[i] + ncol;
+
+                if (r >= 0 && r < image.size() && c >= 0 && c < image[0].size() &&
+                image[r][c] == originalColor){
+                    q.push({r, c});
+                }
+            }
+        }
+
         return image;
     }
 };
