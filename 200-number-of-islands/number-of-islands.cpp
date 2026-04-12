e40b8dec4ec61b66df0c8dd25fd54245a697b4d8
RiddhiSharmaa <Riddhisharma9900@gmail.com>
2026-04-13 00:58:28 +0530

    Time: 40 ms (11.18%) | Memory: 23.1 MB (30.92%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -3,17 +3,25 @@ public:
     void helper(int i, int j, int &cnt, vector<int>& row, vector<int>& col, 
     vector<vector<char>>& grid){
         grid[i][j] = '2';
+        queue<pair<int, int>>q;
+        q.push({i, j});
 
-        for (int k = 0; k < row.size(); k++){
-            int r = i + row[k];
-            int c = j + col[k];
+        while (!q.empty()){
+            int nrow = q.front().first;
+            int ncol = q.front().second;
+            q.pop();
 
-            if (r >= 0 && r < grid.size() && c >= 0 && c < grid[0].size() && 
-            grid[r][c] == '1'){
-                helper(r, c, cnt, row, col, grid);
+            for (int k = 0; k < row.size(); k++){
+                int r = nrow + row[k];
+                int c = ncol + col[k];
+
+                if (r >= 0 && r < grid.size() && c >= 0 && c < grid[0].size() && 
+                grid[r][c] == '1'){
+                    grid[r][c] = '2';
+                    q.push({r, c});
+                }
             }
         }
-        
     }
     int numIslands(vector<vector<char>>& grid) {
         int n = grid.size();
