ad1333ae69e2d8f8050874e4530bb8397c5fa7f3
RiddhiSharmaa <Riddhisharma9900@gmail.com>
2026-04-15 00:37:52 +0530

    Time: 0 ms (100.00%) | Memory: 99.1 MB (86.32%) - LeetSync

--- 1916-find-center-of-star-graph/find-center-of-star-graph.cpp
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.cpp
@@ -1,20 +1,10 @@
 class Solution {
 public:
     int findCenter(vector<vector<int>>& edges) {
-        int n = edges.size()+1;
-        vector<vector<int>>adj(n+1);
-
-        for (auto& edge : edges){
-            int u = edge[0];
-            int v = edge[1];
-            adj[u].push_back(v);
-            adj[v].push_back(u);
+        if (edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1]){
+            return edges[0][0];
         }
 
-        for (int i = 1; i <= n; i++){
-            if (adj[i].size() == n-1) return i;
-        }
-
-        return -1;
+        return edges[0][1];
     }
 };
