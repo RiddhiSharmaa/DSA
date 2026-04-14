0098b79b86cadb0643129adad015e91e3586283d
RiddhiSharmaa <Riddhisharma9900@gmail.com>
2026-04-14 23:57:29 +0530

    Time: 3 ms (39.13%) | Memory: 20.8 MB (26.71%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -26,7 +26,6 @@ public:
             for (int j = 0; j < n; j++){
                 if (isConnected[i][j] == 1){
                     adj[i].push_back(j);
-                    adj[j].push_back(i);
                 }
             }
         }
