5310d23c382a5e0f7f48428fc4ee16e759b1a12f
RiddhiSharmaa <Riddhisharma9900@gmail.com>
2026-04-16 01:02:32 +0530

    Time: 0 ms (100.00%) | Memory: 14.9 MB (18.37%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -2,27 +2,17 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int n = nums.size();
-        vector<int>copy = nums;
-        vector<int>ans;
-        sort(copy.begin(), copy.end());
-        int left = 0, right = n-1;
-
-        while (left < right){
-            if (copy[left] + copy[right] > target){
-                right--;
-            } else if (copy[left] + copy[right] < target){
-                left++;
-            } else {
-                break;
-            }
-        }
-
+        unordered_map<int, int>mpp;
+        
         for (int i = 0; i < n; i++){
-            if (nums[i] == copy[left]) ans.push_back(i);
-            else if (nums[i] == copy[right]) ans.push_back(i);
+            int remaining = target - nums[i];
+            if (mpp.find(remaining) != mpp.end()){
+                return {mpp[remaining], i};
+            }
+
+            mpp[nums[i]] = i;
         }
 
-        if (left < right) return ans;
         return {};
     }
 };
