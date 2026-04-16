b1f0e9aaf884ff35f62b6b867e99fb73e9f090c4
RiddhiSharmaa <Riddhisharma9900@gmail.com>
2026-04-16 23:16:40 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (60.40%) - LeetSync

--- 226-invert-binary-tree/invert-binary-tree.cpp
+++ 226-invert-binary-tree/invert-binary-tree.cpp
@@ -23,10 +23,11 @@ public:
             for (int i = 0; i < size; i++){
                 TreeNode* node = q.front();
                 q.pop();
-                TreeNode* temp1 = (node -> left) ? node -> left : nullptr;
-                TreeNode* temp2 = (node -> right) ? node -> right : nullptr;
-                node -> left = temp2;
-                node -> right = temp1;
+
+                TreeNode* temp = node -> left;
+                node -> left = node -> right;
+                node -> right = temp;
+
                 if (node -> left) q.push(node -> left);
                 if (node -> right) q.push(node -> right);
             }
