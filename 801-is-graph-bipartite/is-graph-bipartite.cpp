b6c64d3f74dc0d019b9db1a7d25d6544d0648376
RiddhiSharmaa <Riddhisharma9900@gmail.com>
2026-05-19 23:40:18 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (14.92%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
-    bool helper(int i, vector<int>& visited, unordered_map<int,int>& mpp, 
-    vector<vector<int>>& graph){
+    bool helper(int i, vector<int>& color, vector<vector<int>>& graph){
         queue<pair<int,int>>q;
         q.push({i, 0});
+        color[i] = 0;
 
         while (!q.empty()){
             int node = q.front().first;
@@ -12,10 +12,9 @@ public:
 
             for (int adj : graph[node]){
                 int c = !col;
-                if (visited[adj] && mpp[adj] != c && mpp[adj] != node) return false;
-                if (!visited[adj]) {
-                    mpp[adj] = c;
-                    visited[adj] = 1;
+                if (color[adj] != -1 && color[adj] != c) return false;
+                if (color[adj] == -1) {
+                    color[adj] = c;
                     q.push({adj, c});
                 }
             }
@@ -25,18 +24,11 @@ public:
     }
 
     bool isBipartite(vector<vector<int>>& graph) {
-        unordered_map<int, int>mpp;
-        vector<int>visited(graph.size(), 0);
+        vector<int>color(graph.size(), -1);
 
         for (int i = 0; i < graph.size(); i++){
-            for (auto v : graph[i]){
-                mpp[i] = v;
-            }
-        }
-
-        for (int i = 0; i < graph.size(); i++){
-            if (!visited[i]){
-                if (!helper(i, visited, mpp, graph)) return false;
+            if (color[i] == -1){
+                if (!helper(i, color, graph)) return false;
             }
         }
 
