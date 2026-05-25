9d170468c70a007054f05228245fd7fa1761ebe9
RiddhiSharmaa <Riddhisharma9900@gmail.com>
2026-05-25 21:42:18 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (5.53%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,25 +1,28 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char>st;
-        
+        stack<int>st;
+
         for (int i = 0; i < s.size(); i++){
-            if (s[i] == '(' || s[i] == '[' || s[i] == '{'){
+            if (s[i] == '(' || s[i] == '{' ||s[i] == '['){
                 st.push(s[i]);
             } else {
-                if (!st.empty() && s[i] == ')' && st.top() == '('){
-                    st.pop();
-                } else if (!st.empty() && s[i] == ']' && st.top() == '['){
-                    st.pop();
-                } else if (!st.empty() && s[i] == '}' && st.top() == '{'){
-                    st.pop();
+                if (!st.empty()){
+                    if (s[i] == ')' && st.top() == '('){
+                        st.pop();
+                    } else if (s[i] == ']' && st.top() == '['){
+                        st.pop();
+                    } else if (s[i] == '}' && st.top() == '{'){
+                        st.pop();
+                    } else {
+                        return false;
+                    }
                 } else {
                     return false;
                 }
             }
         }
-        
-        if (!st.empty()) return false;
-        return true;
+
+        return st.empty();
     }
 };
