0407095621a140bb6eba33705a020b092f61c90a
RiddhiSharmaa <Riddhisharma9900@gmail.com>
2026-05-27 14:43:18 +0530

    Time: 4 ms (21.40%) | Memory: 77.1 MB (43.90%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -11,51 +11,26 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* carry = new ListNode(0);
-        ListNode* dummy = new ListNode(0);
-        ListNode* temp = dummy;
+        ListNode* prev = new ListNode(0);
+        ListNode* dummy = prev;
+        int carry = 0;
 
-        while (l1 && l2){
-            int n = l1 -> val + l2 -> val + carry -> val;
-            int num = n % 10;
-            carry -> val = n / 10;
+        while (l1 != nullptr || l2 != nullptr){
+            int n1 = l1 ? l1 -> val : 0;
+            int n2 = l2 ? l2 -> val : 0;
+            int sum = n1 + n2 + carry;
+            int n = sum % 10;
+            carry = sum / 10;
 
-            ListNode* newNode = new ListNode(num);
-            temp  -> next = newNode;
-            temp = temp -> next;
-
-            l1 = l1 -> next;
-            l2 = l2 -> next;
+            prev -> next = new ListNode(n);
+            prev = prev -> next;
+            if (l1) l1 = l1 -> next;
+            if (l2) l2 = l2 -> next;
         }
 
-        while (l1){
-            int n = l1 -> val + carry -> val;
-            int num = n % 10;
-            carry -> val = n / 10;
-
-            ListNode* newNode = new ListNode(num);
-            temp -> next = newNode;
-            temp = temp -> next;
-
-            l1 = l1 -> next;
-        }
-
-        while (l2){
-            int n = l2 -> val + carry -> val;
-            int num = n % 10;
-            carry -> val = n / 10;
-
-            ListNode* newNode = new ListNode(num);
-            temp  -> next = newNode;
-            temp = temp -> next;
-
-            l2 = l2 -> next;
-        }
-
-        if (carry -> val > 0){
-            ListNode* newNode = new ListNode(carry -> val);
-            temp -> next = newNode;
-            temp = temp -> next;
+        if (carry){
+            prev -> next = new ListNode(carry);
+            prev = prev -> next;
         }
 
         return dummy -> next;
