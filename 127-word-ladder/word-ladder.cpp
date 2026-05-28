8bccd697a922dbacadae9902e01336872fef8afc
RiddhiSharmaa <Riddhisharma9900@gmail.com>
2026-05-28 18:53:36 +0530

    Time: 178 ms (30.66%) | Memory: 21.3 MB (42.81%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -1,33 +1,31 @@
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        unordered_set<string>words(wordList.begin(), wordList.end());
+        unordered_set<string>st(wordList.begin(), wordList.end());
         queue<pair<string, int>>q;
-
         q.push({beginWord, 1});
-        if (words.count(beginWord)) words.erase(beginWord);
 
         while (!q.empty()){
-            string word = q.front().first;
-            int level = q.front().second;
+            auto [word, level] = q.front();
+            st.erase(word);
             q.pop();
-            
-            for (int j = 0; j < word.length(); j++){
-                string replacedWord = word;
-                for (char ch = 'a'; ch <= 'z'; ch++){
-                    replacedWord[j] = ch;
-                    
-                    if (words.count(replacedWord)){
-                        if (replacedWord == endWord){
-                            return level + 1;
-                        }
-                        q.push({replacedWord, level+1});
-                        words.erase(replacedWord);
+
+            for (int i = 0; i < word.size(); i++){
+                for (int j = 0; j < 26; j++){
+                    string w1 = word.substr(0, i);
+                    char w2 = char('a' + j);
+                    string w3 = word.substr(i+1);
+                    string w = w1 + w2 + w3;
+
+                    if (st.find(w) != st.end()){
+                        if (endWord == w) return level+1;
+                        q.push({w, level+1});
+                        st.erase(w);
                     }
                 }
             }
         }
-        
+
         return 0;
     }
 };
