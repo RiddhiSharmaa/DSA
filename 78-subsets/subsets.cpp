3a08e4f1a9b6507ef7cc5e45430c52ec9a4d665f
RiddhiSharmaa <Riddhisharma9900@gmail.com>
2026-05-28 23:19:21 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (57.97%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
-    void helper(int i, vector<int>& nums, vector<int> temp, vector<vector<int>>& ans){
-        if (i >= nums.size()) {
+    void helper(int i, vector<int>& temp, vector<int>& nums, vector<vector<int>>& ans){
+        if (i == nums.size()){
             ans.push_back(temp);
             return;
         }
-        //not pick
-        helper(i+1, nums, temp, ans);
-        //pick
+
         temp.push_back(nums[i]);
+        helper(i+1, temp, nums, ans);
+        temp.pop_back();
+        helper(i+1, temp, nums, ans);
         
-        helper(i+1, nums, temp, ans);
     }
     vector<vector<int>> subsets(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
         vector<vector<int>>ans;
         vector<int>temp;
-        helper(0, nums, temp, ans);
-
+    
+        helper(0, temp, nums, ans);
         return ans;
     }
 };
