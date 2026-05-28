9acc52382b4f6526d79dadbd929c5c4dac5efef0
RiddhiSharmaa <Riddhisharma9900@gmail.com>
2026-05-28 22:55:19 +0530

    Time: 297 ms (25.00%) | Memory: 296.5 MB (18.07%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,27 +1,21 @@
 class Solution {
 public:
-    int cnt = 0;
-    int helper(int i, int prevIdx, vector<int>& nums, vector<vector<int>>& dp){
-        if (i >= nums.size()){
-            return 0;
-        }
-        
-        if (dp[i][prevIdx+1] != -1) return dp[i][prevIdx+1];
-
-        int ans1 = 0, ans2 = 0;
-
-        if (prevIdx == -1 || nums[i] > nums[prevIdx]){
-            ans1 = 1 + helper(i+1, i, nums, dp);
-        }
-
-        ans2 = helper(i+1, prevIdx, nums, dp);
-
-        return dp[i][prevIdx+1] = max(ans1, ans2);
-    }
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>>dp(n, vector<int>(n+1, -1));
+        vector<vector<int>>dp(n+1, vector<int>(n+1, 0));
 
-        return helper(0, -1, nums, dp);
+        for (int i = n-1; i >= 0; i--){
+            for (int prev = i-1; prev >= -1; prev--){
+                int ans1 = INT_MIN;
+                if (prev == -1 || nums[i] > nums[prev]){
+                    ans1 = 1 + dp[i+1][i+1];
+                }
+                int ans2 = dp[i+1][prev+1];
+
+                dp[i][prev+1] = max(ans1, ans2);
+            }
+        }
+
+        return dp[0][0];
     }
 };
