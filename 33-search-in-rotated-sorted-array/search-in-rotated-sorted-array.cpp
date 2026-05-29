3766293588d37065b94a4865d6939d566ab50a15
RiddhiSharmaa <Riddhisharma9900@gmail.com>
2026-05-29 21:58:04 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (66.73%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,43 +1,28 @@
 class Solution {
 public:
-    int binary_search(int low, int high, vector<int>& nums, int target){
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int low = 0, high = n-1;
+
         while (low <= high){
-            int mid = low + (high-low)/2;
-            if (nums[mid] == target){
+            int mid = (low + high)/2;
+            if (nums[mid] == target) {
                 return mid;
-            } else if (nums[mid] < target){
-                low = mid+1;
-            } else if (nums[mid] > target){
-                high = mid-1;
+            } else if (nums[low] <= nums[mid]){
+                if (target < nums[mid] && target >= nums[low]){
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else if (nums[mid] < nums[high]){
+                if (target > nums[mid] && target <= nums[high]){
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
             }
         }
 
         return -1;
-    }
-    int search(vector<int>& nums, int target) {
-        int n = nums.size();
-        int low = 0, high = n-1;
-        int ans = -1;
-
-        while (low <= high){
-            int mid = low + (high - low)/2;
-            if (nums[mid] == target){
-                return mid;
-            } else if (nums[low] <= nums[mid]){
-                if (target >= nums[low] && target <= nums[mid]){
-                    return binary_search(low, mid, nums, target);
-                } else {
-                    low = mid+1;
-                }
-            } else {
-                if (target >= nums[mid] && target <=  nums[high]){
-                    return binary_search(mid, high, nums, target);
-                } else {
-                    high = mid-1;
-                }
-            } 
-        }
-
-        return ans;
     }
 };
