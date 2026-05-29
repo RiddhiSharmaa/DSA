86b41b972c1c51aafe44416e71b5cafa7277488c
RiddhiSharmaa <Riddhisharma9900@gmail.com>
2026-05-29 10:45:25 +0530

    Time: 2 ms (50.20%) | Memory: 8.4 MB (40.96%) - LeetSync

--- 52-n-queens-ii/n-queens-ii.cpp
+++ 52-n-queens-ii/n-queens-ii.cpp
@@ -1,43 +1,44 @@
 class Solution {
 public:
-    bool isSafe(int i, int j, vector<string>& ans){
-        int n = ans.size();
-        for (int r = 0; r < n; r++){
-            if (ans[r][j] == 'Q') return false;
+    bool isSafe(int i, int j, vector<vector<char>>& board){
+        for (int k = i; k >= 0; k--){
+            if (board[k][j] == 'Q') return false;
         }
 
-        for (int c = 0; c < n; c++){
-            if (ans[i][c] == 'Q') return false;
+        int r = i, c = j;
+        while (r >= 0 && c >= 0){
+            if (board[r][c] == 'Q') return false;
+            r--;
+            c--;
         }
 
-        for (int r = i, c = j; r >= 0 && c >= 0; r--, c--){
-            if (ans[r][c] == 'Q') return false;
-        }
-
-        for (int r = i, c = j; r >= 0 && c < n; r--, c++){
-            if (ans[r][c] == 'Q') return false;
+        r = i, c = j;
+        while (r >= 0 && c < board.size()){
+            if (board[r][c] == 'Q') return false;
+            r--;
+            c++;
         }
 
         return true;
     }
-    void helper(int i, vector<string>& ans, int& cnt){
-        if (i == ans.size()) {
+    void helper(int i, int& cnt, vector<vector<char>>& board){
+        if (i == board.size()){
             cnt++;
             return;
         }
 
-        for (int j = 0; j < ans.size(); j++){
-            if (isSafe(i, j, ans)){
-                ans[i][j] = 'Q';
-                helper(i+1, ans, cnt);
-                ans[i][j] = '.';
+        for (int j = 0; j < board.size(); j++){
+            if (isSafe(i, j, board)){
+                board[i][j] = 'Q';
+                helper(i+1, cnt, board);
+                board[i][j] = ',';
             }
         }
     }
     int totalNQueens(int n) {
+        vector<vector<char>>board(n, vector<char>(n, ','));
         int cnt = 0;
-        vector<string>ans(n, string(n, '.'));
-        helper(0, ans, cnt);
+        helper(0, cnt, board);
 
         return cnt;
     }
