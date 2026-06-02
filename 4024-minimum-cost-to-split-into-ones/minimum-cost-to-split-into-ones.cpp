abfa27c914714de113c1c73c1029659b769c5373
RiddhiSharmaa <Riddhisharma9900@gmail.com>
2026-06-02 14:33:29 +0530

    Time: 4 ms (23.74%) | Memory: 15.3 MB (12.22%) - LeetSync

--- 4024-minimum-cost-to-split-into-ones/minimum-cost-to-split-into-ones.cpp
+++ 4024-minimum-cost-to-split-into-ones/minimum-cost-to-split-into-ones.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
-    int minCost(int n) {
-        if (n == 1) return 0;
-        if (n == 2) return 1;
-        
+    int helper(int i, vector<int>& dp){
+        if (i == 1) return 0;
+        if (i == 2) return 1;
+        if (dp[i] != -1) return dp[i];
+
         int cost = 0;
-        if (n % 2 != 0){
-            cost = n-1 + minCost(n-1);
+        if (i % 2 != 0){
+            cost = i-1 + minCost(i-1);
         } else {
-            cost = n/2 * n/2 + 2 * minCost(n/2);
+            cost = i/2 * i/2 + 2 * minCost(i/2);
         }
 
-        return cost;
+        return dp[i] = cost;
+    }
+
+    int minCost(int n) {
+        vector<int>dp(n+1, -1);
+        return helper(n, dp);
     }
 };
