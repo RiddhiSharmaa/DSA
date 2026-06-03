005768bba50936e1f9b5218342477fd428e68b00
RiddhiSharmaa <Riddhisharma9900@gmail.com>
2026-06-03 17:02:41 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (94.79%) - LeetSync

--- 1381-maximum-score-words-formed-by-letters/maximum-score-words-formed-by-letters.cpp
+++ 1381-maximum-score-words-formed-by-letters/maximum-score-words-formed-by-letters.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
-    int helper(int i, vector<string>& words, unordered_map<char, int>& mpp1,
-    unordered_map<char, int>& mpp2){
+    int mpp1[26] = {0};
+    int mpp2[26] = {0};
+    int helper(int i, vector<string>& words){
         if (i >= words.size()) return 0;
 
         int s = 0;
         bool flag = true;
         for (int j = 0; j < words[i].size(); j++){
-            if (mpp2[words[i][j]] > 0){
-                s += mpp1[words[i][j]];
-                mpp2[words[i][j]]--;
+            if (mpp2[words[i][j]-'a'] > 0){
+                s += mpp1[words[i][j]-'a'];
+                mpp2[words[i][j]-'a']--;
             } else {
                 s = 0;
                 flag = false;
                 int k = j-1;
                 while (k >= 0){
-                    mpp2[words[i][k]]++;
+                    mpp2[words[i][k]-'a']++;
                     k--;
                 }
                 break;
@@ -24,29 +25,26 @@ public:
 
         int take = 0;
         if (flag){
-            take = s + helper(i+1, words, mpp1, mpp2);
+            take = s + helper(i+1, words);
             for (char c : words[i]){
-                mpp2[c]++;
+                mpp2[c-'a']++;
             }
         }
    
-        int notTake = helper(i+1, words, mpp1, mpp2);
+        int notTake = helper(i+1, words);
 
         return max(take, notTake);
     }
     int maxScoreWords(vector<string>& words, vector<char>& letters, vector<int>& score) {
         int n = words.size();
-        unordered_map<char, int>mpp1, mpp2;
-
+    
         for (int i = 0; i < 26; i++){
-            mpp1[i+'a'] = score[i];
+            mpp1[i] = score[i];
         }
 
         for (char c : letters){
-            mpp2[c]++;
+            mpp2[c-'a']++;
         }
-
-        // vector<vector<int>>dp(n+1, vector<int>())
-        return helper(0, words, mpp1, mpp2);
+        return helper(0, words);
     }
 };
