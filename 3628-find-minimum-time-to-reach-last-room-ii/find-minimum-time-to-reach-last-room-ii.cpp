d170eb2821c642ee5a67ff98531f1417edf2553c
RiddhiSharmaa <Riddhisharma9900@gmail.com>
2026-06-04 23:57:35 +0530

    Time: 179 ms (94.68%) | Memory: 96.3 MB (76.37%) - LeetSync

---
+++ 3628-find-minimum-time-to-reach-last-room-ii/find-minimum-time-to-reach-last-room-ii.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    int minTimeToReach(vector<vector<int>>& moveTime) {
+        int n = moveTime.size();
+        int m = moveTime[0].size();
+        vector<vector<int>>dist(n, vector<int>(m, INT_MAX));
+
+        priority_queue<pair<pair<int, int>, pair<int, int>>, 
+        vector<pair<pair<int, int>, pair<int, int>>>, 
+        greater<pair<pair<int, int>, pair<int, int>>>>pq;
+
+        vector<int>row = {-1, 0, 1, 0};
+        vector<int>col = {0, 1, 0, -1};
+        
+        int time = moveTime[0][0];
+        pq.push({{0, 1}, {0, 0}});
+        dist[0][0] = 0;
+
+        while (!pq.empty()){
+            int t = pq.top().first.first;
+            int alt = pq.top().first.second;
+            int x = pq.top().second.first;
+            int y = pq.top().second.second;
+
+            pq.pop();
+
+            if (x == n-1 && y == m-1) return t;
+
+            for (int i = 0; i < 4; i++){
+                int r = row[i] + x;
+                int c = col[i] + y;
+
+                if (r >= 0 && r < n && c >= 0 && c < m){
+                    int a = 0;
+
+                    if (t <= moveTime[r][c]){
+                        a = moveTime[r][c] + alt;
+                    } else {
+                        a = t + alt;
+                    }
+                    if (dist[r][c] > a){
+                        dist[r][c] = a;
+                        int b = (alt == 1) ? 2 : 1;
+                        pq.push({{dist[r][c], b}, {r, c}});
+                    }
+                }
+            }
+        }
+
+        return -1;
+    }
+};
