ae244b6ad38db25c75413b51711a4f5d66e89a87
RiddhiSharmaa <Riddhisharma9900@gmail.com>
2026-06-18 16:39:51 +0530

    Time: 39 ms (44.90%) | Memory: 13.7 MB (96.57%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,29 +1,22 @@
 class Solution {
 public:
+    int dp[13][10001];
+
+    int helper(int i, vector<int>& coins, int target){
+        if (target == 0) return 0;
+        if (i >= coins.size() || target < 0) return 1e9;
+        if (dp[i][target] != -1) return dp[i][target];
+
+        int notPick = helper(i+1, coins, target);
+        int pick = 1 + helper(i, coins, target - coins[i]);
+
+        return dp[i][target] = min(pick, notPick);
+    }
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>>dp(n+1, vector<int>(amount+1, -1));
+        memset(dp, -1, sizeof(dp));
+        int ans = helper(0, coins, amount);
 
-        for (int i = 0; i <= n; i++){
-            dp[i][0] = 0;
-        }
-        for (int j = 1; j <= amount; j++){
-            dp[n][j] = 1e9;
-        }
-
-        for (int i = n-1; i >= 0; i--){
-            for (int j = 0; j <= amount; j++){
-                int notPick = dp[i+1][j];
-                int pick = 1e9;
-                if (j - coins[i] >= 0){
-                    pick = 1 + dp[i][j - coins[i]];
-                }
-                dp[i][j] = min(pick, notPick);
-            }
-        }
-
-        int ans = dp[0][amount];
-
-        return ans >= 1e9 ? -1 : ans;
+        return (ans >= 1e9) ? -1 : ans;
     }
 };
