e2f62ef5215c007eef03f7a8e87e663b7e96dddd
RiddhiSharmaa <Riddhisharma9900@gmail.com>
2026-06-19 20:01:33 +0530

    Time: 90 ms (82.93%) | Memory: 19.1 MB (92.20%) - LeetSync

--- 3629-total-characters-in-string-after-transformations-i/total-characters-in-string-after-transformations-i.cpp
+++ 3629-total-characters-in-string-after-transformations-i/total-characters-in-string-after-transformations-i.cpp
@@ -3,27 +3,26 @@ public:
     int lengthAfterTransformations(string s, int t) {
         long long len = 0;
         long long MOD = 1e9 + 7;
-        vector<int>v(26, 0);
+        vector<int>freq(26, 0);
 
         for (int i = 0; i < s.size(); i++){
-            v[s[i]-'a']++;
+            freq[s[i]-'a']++;
         }
 
         while (t--){
-            vector<int>temp(26, 0);
-            for (int i = 0; i < 26; i++){
-                if (i != 25){
-                    temp[i+1] += v[i];
-                } else {
-                    temp[0] += (v[25] % MOD);
-                    temp[1] += (v[25] % MOD);
-                }
+            int zCnt = freq[25];
+            for (int i = 25; i > 0; i--){
+                freq[i] = freq[i-1];
+                freq[i-1] = 0;
             }
-            v = temp;
+            if (zCnt > 0){
+                freq[0] += (zCnt % MOD);
+                freq[1] += (zCnt % MOD);
+            }
         }
 
         for (int i = 0; i < 26; i++){
-            len += v[i];
+            len += freq[i];
         }
 
         return len % MOD;
