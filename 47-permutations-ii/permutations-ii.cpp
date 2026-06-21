7d8ee5bc9a67833a25c01c1ed0a0b8aeebfa5c89
RiddhiSharmaa <Riddhisharma9900@gmail.com>
2026-06-21 10:30:15 +0530

    Time: 3 ms (61.25%) | Memory: 11.4 MB (79.71%) - LeetSync

--- 47-permutations-ii/permutations-ii.cpp
+++ 47-permutations-ii/permutations-ii.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
-    void helper(vector<int>& temp, vector<int>& visited, vector<int>& nums, vector<vector<int>>& ans){
+    void helper(int idx, vector<int>& temp, vector<int>& visited, vector<int>& nums, vector<vector<int>>& ans){
         if (temp.size() == nums.size()){
             ans.push_back(temp);
             return;
         }
 
-        for (int j = 0; j < nums.size(); j++){
-            if (j > 0 && nums[j] == nums[j-1] && !visited[j-1]) continue;
-            if (visited[j]) continue;
-            visited[j] = 1;
-            temp.push_back(nums[j]);
-            helper(temp, visited, nums, ans);
+        for (int i = 0; i < nums.size(); i++){
+            if (i > 0 && nums[i] == nums[i-1] && !visited[i-1]) continue;
+            if (visited[i]) continue;
+            visited[i] = 1;
+            temp.push_back(nums[i]);
+            helper(i+1, temp, visited, nums, ans);
+            visited[i] = 0;
             temp.pop_back();
-            visited[j] = 0;
         }
     }
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-        int n = nums.size();
         sort(nums.begin(), nums.end());
         vector<vector<int>>ans;
         vector<int>temp;
-        vector<int>visited(n, 0);
-        helper(temp, visited, nums, ans);
-
+        vector<int>visited(nums.size(), 0);
+        helper(0, temp, visited, nums, ans);
         return ans;
     }
 };
