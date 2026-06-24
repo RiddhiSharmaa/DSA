b94e0e76f1096d124dbde3bb0a765e7c918dffc6
RiddhiSharmaa <Riddhisharma9900@gmail.com>
2026-06-24 16:11:59 +0530

    Time: 7 ms (33.12%) | Memory: 11.6 MB (32.58%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -1,6 +1,18 @@
 class Solution {
 public:
+    int dp[501][501];
+    int helper(int l, int r, vector<int>& piles){
+        if (l > r) return 0;
+
+        if (dp[l][r] != -1) return dp[l][r];
+
+        int takeLeft = piles[l] - helper(l+1, r, piles);
+        int takeRight = piles[r] - helper(l, r-1, piles);
+
+        return dp[l][r] = max(takeLeft, takeRight);
+    }
     bool stoneGame(vector<int>& piles) {
-        return true;
+        memset(dp, -1, sizeof(dp));
+        return helper(0, piles.size()-1, piles) > 0;
     }
 };
