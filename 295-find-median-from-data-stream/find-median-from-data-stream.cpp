448ea23c3166082b1b8c7aba7639cec2796a6fd7
RiddhiSharmaa <Riddhisharma9900@gmail.com>
2026-06-26 23:55:06 +0530

    Time: 56 ms (89.58%) | Memory: 148.5 MB (63.56%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.cpp
+++ 295-find-median-from-data-stream/find-median-from-data-stream.cpp
@@ -7,30 +7,40 @@ public:
     }
     
     void addNum(int num) {
-        if (maxH.empty() || num < maxH.top()){
+        if (maxH.empty() || (!minH.empty() && num <= minH.top())){
             maxH.push(num);
-        } else {
+        } else if (minH.empty() || (!maxH.empty() && num > maxH.top())){
             minH.push(num);
         }
 
-        if (minH.size() > maxH.size()+1){
+        if (!maxH.empty() && !minH.empty() && maxH.top() > minH.top()){
+            minH.push(maxH.top());
+            maxH.pop();
+        }
+        if (!maxH.empty() && !minH.empty() && minH.top() < maxH.top()){
             maxH.push(minH.top());
             minH.pop();
         }
 
-        if (maxH.size() > minH.size()+1){
+        if (maxH.size()+1 < minH.size()){
+            maxH.push(minH.top());
+            minH.pop();
+        }
+
+        if (minH.size()+1 < maxH.size()){
             minH.push(maxH.top());
             maxH.pop();
         }
     }
     
     double findMedian() {
-        if (maxH.size() == minH.size()){
-            return (maxH.top() + minH.top())/2.0;
-        } else if (maxH.size() > minH.size()){
+        if (minH.size() == maxH.size()){
+            return (minH.top() + maxH.top()) / 2.0;
+        } else if (minH.size() > maxH.size()){
+            return minH.top();
+        } else {
             return maxH.top();
         }
-        return minH.top();
     }
 };
 
