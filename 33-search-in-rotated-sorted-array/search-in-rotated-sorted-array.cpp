517fa20dd10ecf166e73d719905acbe89e35dc53
RiddhiSharmaa <Riddhisharma9900@gmail.com>
2026-06-28 18:54:03 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (34.32%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -5,20 +5,20 @@ public:
         int low = 0, high = n-1;
 
         while (low <= high){
-            int mid = (low + high)/2;
-            if (nums[mid] == target) {
+            int mid = low + (high - low)/2;
+            if (nums[mid] == target){
                 return mid;
             } else if (nums[low] <= nums[mid]){
-                if (target < nums[mid] && target >= nums[low]){
-                    high = mid - 1;
+                if (nums[low] <= target && nums[mid] >= target){
+                    high = mid-1;
                 } else {
-                    low = mid + 1;
+                    low = mid+1;
                 }
-            } else if (nums[mid] < nums[high]){
-                if (target > nums[mid] && target <= nums[high]){
-                    low = mid + 1;
+            } else if (nums[mid] <= nums[high]){
+                if (nums[mid] <= target && nums[high] >= target){
+                    low = mid+1;
                 } else {
-                    high = mid - 1;
+                    high = mid-1;
                 }
             }
         }
