67ea8118c0e2d342970106fc13127bb6be4a860d
RiddhiSharmaa <Riddhisharma9900@gmail.com>
2026-07-01 02:05:07 +0530

    Time: 88 ms (78.95%) | Memory: 106.1 MB (65.41%) - LeetSync

--- 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
-    void dfs(int node, int& cnt, vector<int>& visited, 
-    vector<vector<pair<int, int>>>& adj){
+    void dfs(int node, int& cnt, vector<int>& visited, vector<vector<int>>& adj){
         visited[node] = 1;
 
         for (auto& p : adj[node]){
-            if (!visited[p.first]){
-                cnt += p.second;
-                dfs(p.first, cnt, visited, adj);
+            int n = abs(p);
+            if (!visited[n]){
+                if (p < 0) cnt++;
+                dfs(n, cnt, visited, adj);
             }
         }
     }
     int minReorder(int n, vector<vector<int>>& connections) {
-        vector<vector<pair<int, int>>>adj(n);
+        vector<vector<int>>adj(n);
         vector<int>visited(n, 0);
         int cnt = 0;
 
         for (auto& c : connections){
             int u = c[0];
             int v = c[1];
-            adj[u].push_back({v, 1});
-            adj[v].push_back({u, 0});
+            adj[u].push_back(-v);
+            adj[v].push_back(u);
         }
-
+        
         dfs(0, cnt, visited, adj);
 
         return cnt;
