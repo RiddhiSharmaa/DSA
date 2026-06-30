08b7998d8f74cd9fbb04dc50cdf12cb3a6455a90
RiddhiSharmaa <Riddhisharma9900@gmail.com>
2026-06-30 19:18:14 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (79.91%) - LeetSync

---
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        while (true){
+            if (s[s.size()-1] == ' ') s.pop_back();
+            else break;
+        }
+        reverse(s.begin(), s.end());
+        while (true){
+            if (s[s.size()-1] == ' ') s.pop_back();
+            else break;
+        }
+        cout << s << endl;
+        string w = "";
+        string ans = "";
+
+        for (int i = 0; i < s.size(); i++){
+            if (s[i] == ' '){
+                reverse(w.begin(), w.end());
+                ans += w;
+                if (i > 0 && s[i-1] != ' ') ans += ' ';
+                w = "";
+                continue;
+            }
+            w += s[i];
+        }
+
+        reverse(w.begin(), w.end());
+        ans += w;
+        return ans;
+    }
+};
