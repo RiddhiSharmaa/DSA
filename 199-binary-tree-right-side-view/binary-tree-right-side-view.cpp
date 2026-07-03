cbc0311529bc13aa58896eb5dcd5bffe472806c4
RiddhiSharmaa <Riddhisharma9900@gmail.com>
2026-07-04 00:32:01 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (41.48%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -11,15 +11,23 @@
  */
 class Solution {
 public:
-    void helper(TreeNode* root, int level, vector<int>& ans){
-        if (!root) return;
-        if (ans.size()+1 == level) ans.push_back(root -> val);
-        helper(root -> right, level+1, ans);
-        helper(root -> left, level+1, ans);
-    }
     vector<int> rightSideView(TreeNode* root) {
+        if (!root) return {};
         vector<int>ans;
-        helper(root, 1, ans);
+        queue<TreeNode*>q;
+        q.push(root);
+
+        while (!q.empty()){
+            int n = q.size();
+            
+            for (int i = 0; i < n; i++){
+                TreeNode* node = q.front();
+                q.pop();
+                if (i == n-1) ans.push_back(node -> val);
+                if (node -> left) q.push(node -> left);
+                if (node -> right) q.push(node -> right);
+            }
+        }
 
         return ans;
     }
