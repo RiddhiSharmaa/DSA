d008751dd5186bc103423a42b4cd1958ba6111f1
RiddhiSharmaa <Riddhisharma9900@gmail.com>
2026-07-14 15:13:19 +0530

    Time: 33 ms (20.99%) | Memory: 23.7 MB (25.56%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -3,16 +3,26 @@ public:
     int dr[4] = {-1, 0, 1, 0};
     int dc[4] = {0, 1, 0, -1};
     int vis[301][301];
-    void dfs(int i, int j, vector<vector<char>>& grid){
+    void bfs(int i, int j, vector<vector<char>>& grid){
+        queue<pair<int, int>>q;
+        q.push({i, j});
         vis[i][j] = 1;
 
-        for (int k = 0; k < 4; k++){
-            int r = dr[k] + i;
-            int c = dc[k] + j;
-            if (r >= 0 && r < grid.size() && c >= 0 && c < grid[0].size() &&
-            grid[r][c] == '1' && !vis[r][c]){
-                dfs(r, c, grid);
+        while (!q.empty()){
+            int x = q.front().first;
+            int y = q.front().second;
+            q.pop();
+
+            for (int k = 0; k < 4; k++){
+                int r = dr[k] + x;
+                int c = dc[k] + y;
+                if (r >= 0 && r < grid.size() && c >= 0 && c < grid[0].size() &&
+                grid[r][c] == '1' && !vis[r][c]){
+                    q.push({r, c});
+                    vis[r][c] = 1;
+                }
             }
+
         }
     }
     int numIslands(vector<vector<char>>& grid) {
@@ -24,7 +34,7 @@ public:
         for (int i = 0; i < n; i++){
             for (int j = 0; j < m; j++){
                 if (grid[i][j] == '1' && !vis[i][j]){
-                    dfs(i, j, grid);
+                    bfs(i, j, grid);
                     cnt++;
                 }
             }
