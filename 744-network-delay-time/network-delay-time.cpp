c014adbed7b116086a4909424ca5a0fa8b3f28ba
RiddhiSharmaa <Riddhisharma9900@gmail.com>
2026-07-14 20:13:14 +0530

    Time: 87 ms (44.52%) | Memory: 44.3 MB (82.49%) - LeetSync

--- 744-network-delay-time/network-delay-time.cpp
+++ 744-network-delay-time/network-delay-time.cpp
@@ -3,49 +3,40 @@ public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
         vector<int>time(n+1, 1e9);
         vector<vector<pair<int, int>>>adj(n+1);
+        int minTime = INT_MIN;
 
-        for (auto& edge : times){
-            int u = edge[0];
-            int v = edge[1];
-            int w = edge[2];
-
+        for (auto& t : times){
+            int u = t[0];
+            int v = t[1];
+            int w = t[2];
             adj[u].push_back({v, w});
         }
 
-        priority_queue<pair<int, int>,
-                        vector<pair<int, int>>,
-                        greater<pair<int, int>>>pq;
-
-        int minTime = 0;
-        time[k] = 0;
+        priority_queue<pair<int, int>, vector<pair<int, int>>, 
+        greater<pair<int, int>>>pq;
         pq.push({0, k});
+        time[k] = 0;
 
         while (!pq.empty()){
+            int t = pq.top().first;
             int node = pq.top().second;
-            int currTime = pq.top().first;
             pq.pop();
+            if (t > time[node]) continue;
+            minTime = max(minTime, t);
 
-            for (auto& neigh : adj[node]){
-                int adjNode = neigh.first;
-                int adjWt = neigh.second;
-
-                if (currTime + adjWt < time[adjNode]){
-                    time[adjNode] = currTime + adjWt;
+            for (auto& p : adj[node]){
+                int adjNode = p.first;
+                int adjTime = p.second;
+                if (adjTime + t < time[adjNode]){
+                    time[adjNode] = adjTime + t;
                     pq.push({time[adjNode], adjNode});
                 }
             }
         }
 
-        int ans = INT_MIN;
-
-        for (int i = 1; i < time.size(); i++){
-            if (time[i] == 1e9){
-                return -1;
-            } else{
-                ans = max(ans, time[i]);
-            }
+        for (int i = 1; i <= n; i++){
+            if (time[i] == 1e9) return -1;
         }
-
-        return ans;
+        return minTime;
     }
 };
