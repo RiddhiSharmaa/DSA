7008da15fdfadc8250253705cf0943d34a97de75
RiddhiSharmaa <Riddhisharma9900@gmail.com>
2026-07-16 22:45:30 +0530

    Time: 947 ms (11.27%) | Memory: 243 MB (8.97%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
@@ -1,45 +1,45 @@
 class Solution {
 public:
     int minCostConnectPoints(vector<vector<int>>& points) {
-        vector<vector<pair<int, int>>> adj(points.size());
-        vector<int>visited(points.size(), 0);
+        int n = points.size();
+        priority_queue<pair<int, int>, vector<pair<int, int>>,
+        greater<pair<int, int>>>pq;
+        vector<int>vis(n, 0);
+        vector<vector<pair<int, int>>>adj(n);
 
-        for (int i = 0; i < points.size(); i++){
-            for (int j = i+1; j < points.size(); j++){
-                int Mdist = abs(points[i][0] - points[j][0]) +
-                abs(points[i][1] - points[j][1]);
-
-                adj[i].push_back({j, Mdist});
-                adj[j].push_back({i, Mdist});
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < n; j++){
+                int x1 = points[i][0];
+                int x2 = points[j][0];
+                int y1 = points[i][1];
+                int y2 = points[j][1];
+                int dist = abs(x1-x2) + abs(y1-y2);
+                adj[i].push_back({dist, j});
+                adj[j].push_back({dist, i});
             }
         }
 
-        priority_queue<pair<int, int>, vector<pair<int, int>>, 
-        greater<pair<int, int>>>pq;
-
+        int minCost = 0;
         pq.push({0, 0});
 
-        int ans = 0;
-
         while (!pq.empty()){
-            int node = pq.top().second;
-            int dist = pq.top().first;
+            int d = pq.top().first;
+            int n = pq.top().second;
             pq.pop();
 
-            if (visited[node]) continue;
+            if (vis[n]) continue;
+            minCost += d;
+            vis[n] = 1;
 
-            visited[node] = 1;
-            ans += dist;
-
-            for (auto& neigh : adj[node]){
-                int adjNode = neigh.first;
-                int adjWt = neigh.second;
-                if (!visited[adjNode]){
-                    pq.push({adjWt, adjNode});
+            for (auto& p : adj[n]){
+                int neigh = p.second;
+                int dist = p.first;
+                if (!vis[neigh]){
+                    pq.push({dist, neigh});
                 }
             }
         }
 
-        return ans;
+        return minCost;
     }
 };
