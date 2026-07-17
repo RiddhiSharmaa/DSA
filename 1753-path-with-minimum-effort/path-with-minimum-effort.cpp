5c7603335746225241e15da7f751f26ea5cfcd4f
RiddhiSharmaa <Riddhisharma9900@gmail.com>
2026-07-17 10:28:34 +0530

    Time: 57 ms (52.43%) | Memory: 24.4 MB (77.05%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
@@ -5,33 +5,33 @@ public:
         int m = heights[0].size();
         priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>,
         greater<pair<int, pair<int, int>>>>pq;
-        vector<vector<int>>vis(n, vector<int>(m, 0));
+        vector<vector<int>>diff(n, vector<int>(m, 1e9));
         vector<int>row = {-1, 0, 1, 0};
         vector<int>col = {0, 1, 0, -1};
         pq.push({0, {0, 0}});
-        int effort = 0;
+        diff[0][0] = 0;
 
         while (!pq.empty()){
-            int d = pq.top().first;
+            int e = pq.top().first;
             int x = pq.top().second.first;
             int y = pq.top().second.second;
             pq.pop();
 
-            if (vis[x][y]) continue;
-            vis[x][y] = 1;
-            effort = max(effort, d);
-            if (x == n-1 && y == m-1) break;
+            if (x == n-1 && y == m-1) return e;
 
             for (int i = 0; i < 4; i++){
                 int r = x + row[i];
                 int c = y + col[i];
-                if (r >= 0 && r < n && c >= 0 && c < m && !vis[r][c]){
-                    int diff = abs(heights[x][y] - heights[r][c]);
-                    pq.push({diff, {r, c}});
+                if (r >= 0 && r < n && c >= 0 && c < m){
+                    int difference = max(abs(heights[x][y] - heights[r][c]), e);
+                    if (difference < diff[r][c]){
+                        diff[r][c] = difference;
+                        pq.push({difference, {r, c}});
+                    }
                 }
             }
         }
 
-        return effort;
+        return -1;
     }
 };
