c003a5844f6fd25a32cc9972951d10b24bcd37e4
RiddhiSharmaa <Riddhisharma9900@gmail.com>
2026-07-17 10:08:29 +0530

    Time: 43 ms (86.63%) | Memory: 26.4 MB (29.51%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
@@ -1,45 +1,37 @@
 class Solution {
 public:
-    typedef pair<int, pair<int, int>> P;
     int minimumEffortPath(vector<vector<int>>& heights) {
         int n = heights.size();
         int m = heights[0].size();
-
-        vector<vector<int>>visited(n, vector<int>(m, 0));
-        vector<vector<int>>diff(n, vector<int>(m, 1e9));
-        priority_queue<P, vector<P>, greater<P>>pq;
-
-        int rows[] = {-1, 0, 1, 0};
-        int cols[] = {0, 1, 0, -1};
-
+        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>,
+        greater<pair<int, pair<int, int>>>>pq;
+        vector<vector<int>>vis(n, vector<int>(m, 0));
+        vector<int>row = {-1, 0, 1, 0};
+        vector<int>col = {0, 1, 0, -1};
         pq.push({0, {0, 0}});
-        diff[0][0] = 0;
+        int effort = 0;
 
         while (!pq.empty()){
+            int d = pq.top().first;
             int x = pq.top().second.first;
             int y = pq.top().second.second;
-            int effort = pq.top().first;
             pq.pop();
 
-            if (effort > diff[x][y]) continue;
-            if (x == n-1 && y == m-1) return effort;
-            if (visited[x][y]) continue;
-            visited[x][y] = 1;
+            if (vis[x][y]) continue;
+            vis[x][y] = 1;
+            effort = max(effort, d);
+            if (x == n-1 && y == m-1) break;
 
             for (int i = 0; i < 4; i++){
-                int r = rows[i] + x;
-                int c = cols[i] + y;
-
-                if (r >= 0 && r < n && c >= 0 && c < m && !visited[r][c]){
-                    int currMin = max(abs(heights[r][c] - heights[x][y]), effort);
-                    if (currMin < diff[r][c]){
-                        diff[r][c] = currMin;
-                        pq.push({currMin, {r, c}});
-                    }
+                int r = x + row[i];
+                int c = y + col[i];
+                if (r >= 0 && r < n && c >= 0 && c < m && !vis[r][c]){
+                    int diff = abs(heights[x][y] - heights[r][c]);
+                    pq.push({diff, {r, c}});
                 }
             }
         }
 
-        return -1;
+        return effort;
     }
 };
