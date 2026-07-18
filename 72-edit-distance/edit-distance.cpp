2f5e9ab410286c3e9f078aca75c814f8225aaa06
RiddhiSharmaa <Riddhisharma9900@gmail.com>
2026-07-18 19:17:51 +0530

    Time: 39 ms (13.24%) | Memory: 11.9 MB (84.73%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     int dp[501][501];
     int helper(int i, int j, string& word1, string& word2){
-        if (j >= word2.size() && i < word1.size()) return word1.size()-i;
-        if (i >= word1.size() && j < word2.size()) return word2.size()-j;
+        if (j >= word2.size() && i < word1.size()) return dp[i][j] = word1.size()-i;
+        if (i >= word1.size() && j < word2.size()) return dp[i][j] = word2.size()-j;
         if (i >= word1.size() || j >= word2.size()) return 0;
         if (dp[i][j] != -1) return dp[i][j];
 
