be823a0b4eda80f36a7c56885d50eaf9db3f825f
RiddhiSharmaa <Riddhisharma9900@gmail.com>
2026-07-22 01:06:37 +0530

    Time: 8 ms (59.49%) | Memory: 11.8 MB (75.16%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
-    int helper(int i, int target, int &sum, vector<int>& nums, vector<vector<int>>& dp){
-        if (i == nums.size() && target == 0) return 1;
-        if (i >= nums.size()) return 0;
-        if (target > sum || target < -sum) return 0;
-
-        if (dp[i][target+sum] != -1) return dp[i][target+sum];
-
-        int sub = helper(i+1, target + nums[i], sum, nums, dp);
-        int add = helper(i+1, target - nums[i], sum, nums, dp);
-
-        return dp[i][target+sum] = add + sub;
+    int dp[21][2001];
+    int helper(int i, int target, int& t, vector<int>& nums){
+        if (i == nums.size()){
+            if (target == t) return 1;
+            return 0;
+        }
+        if (i > nums.size()) return 0;
+        if (dp[i][1000+target] != -1) return dp[i][1000+target];
+        int pos = helper(i+1, target+nums[i], t, nums);
+        int neg = helper(i+1, target-nums[i], t, nums);
+        return dp[i][1000+target] = pos + neg;
     }
-
     int findTargetSumWays(vector<int>& nums, int target) {
-        int n = nums.size(), sum = 0;
-        for (int i = 0; i < nums.size(); i++) sum += nums[i];
-        vector<vector<int>>dp(n, vector<int>((2*sum)+1, -1));
-        return helper(0, target, sum, nums, dp);
+        memset(dp, -1, sizeof(dp));
+        return helper(0, 0, target, nums);
     }
 };
