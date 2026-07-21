23126af0a63f59287d0fa765c2bdc82c99aa26dd
RiddhiSharmaa <Riddhisharma9900@gmail.com>
2026-07-21 17:47:48 +0530

    Time: 189 ms (6.75%) | Memory: 153.6 MB (14.86%) - LeetSync

--- 3805-maximize-active-section-with-trade-i/maximize-active-section-with-trade-i.cpp
+++ 3805-maximize-active-section-with-trade-i/maximize-active-section-with-trade-i.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int maxActiveSectionsAfterTrade(string s) {
         int n = s.size();
-        vector<string>v;
+        vector<pair<char, int>>v;
         string temp = "";
         temp += s[0];
         int maxi = 0;
@@ -11,20 +11,21 @@ public:
         for (int i = 1; i < n; i++){
             if (s[i] == temp.back()) temp += s[i];
             else {
-                v.push_back(temp);
+                v.push_back({temp.back(), temp.size()});
                 temp = "";
                 temp = s[i];
             }
         }
-        if (temp.size() > 0) v.push_back(temp);
+        if (temp.size() > 0) v.push_back({temp.back(), temp.size()});
 
         for (int i = 0; i < v.size(); i++){
-            if (v[i][0] == '1'){
-                if (i >= 1 && i < v.size()-1 && v[i-1][0] == '0' && v[i+1][0] == '0'){
-                    int m = v[i+1].size() + v[i-1].size();
+            if (v[i].first == '1'){
+                if (i >= 1 && i < v.size()-1 && v[i-1].first == '0' && 
+                v[i+1].first == '0'){
+                    int m = v[i+1].second + v[i-1].second;
                     maxi = max(maxi, m);
                 }
-                active += v[i].size();
+                active += v[i].second;
             } 
         }
 
