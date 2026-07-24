a684533a4a46b0bcb22d42f20e5a8e7fe64ae06d
RiddhiSharmaa <Riddhisharma9900@gmail.com>
2026-07-24 17:12:24 +0530

    Time: 19 ms (15.22%) | Memory: 17.1 MB (25.55%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
     int dp[301];
-    bool helper(int i, string& s, unordered_set<string>& st){
+    bool helper(int i, string& s, unordered_set<string>&st){
         if (i >= s.size()) return true;
         if (dp[i] != -1) return dp[i];
-
         for (int k = i; k < s.size(); k++){
             string str = s.substr(i, k-i+1);
-            if (st.count(str)){
+            if (st.find(str) != st.end()){
                 if (helper(k+1, s, st)) return dp[i] = true;
             }
         }
-
         return dp[i] = false;
     }
     bool wordBreak(string s, vector<string>& wordDict) {
-        unordered_set<string>st(wordDict.begin(), wordDict.end());
         memset(dp, -1, sizeof(dp));
-
+        unordered_set<string>st(wordDict.begin(), wordDict.end());
         return helper(0, s, st);
-
     }
 };
