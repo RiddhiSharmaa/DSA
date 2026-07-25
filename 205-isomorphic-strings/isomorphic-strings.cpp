1ef0141f474bbb821ce1db7f8f730ed9f89ff3da
RiddhiSharmaa <Riddhisharma9900@gmail.com>
2026-07-25 17:42:22 +0530

    Time: 2 ms (48.98%) | Memory: 9.6 MB (7.69%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -6,14 +6,13 @@ public:
         for (int i = 0; i < s.size(); i++){
             char c = s[i];
             char d = t[i];
-            if (mpp.find(c) == mpp.end() && mpp2.find(d) == mpp2.end()){
+            if (mpp.find(c) == mpp.end()){
                 mpp[c] = d;
-                mpp2[d] = c;
-            } else if (mpp2.find(d) != mpp2.end()){
-                if (mpp2[d] != c) return false;
-            } else {
-                if (d != mpp[c]) return false;
             }
+            if (mpp2.find(d) == mpp2.end()){
+                mpp2[d] = c;
+            }
+            if (mpp2[d] != c || d != mpp[c]) return false;
         }
 
         return true;
