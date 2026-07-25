aa89f6d7b93301c013f71af8b0163ced29547560
RiddhiSharmaa <Riddhisharma9900@gmail.com>
2026-07-25 09:41:19 +0530

    Time: 24 ms (97.18%) | Memory: 53.5 MB (72.67%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
-    bool palindrome(string& s){
-        int n = s.size();
-        for (int i = 0; i < n/2; i++){
-            if (s[i] != s[n-i-1]) return false;
-        }
-        return true;
-    }
-    void helper(int idx, vector<string>& temp, string& s, vector<vector<string>>& ans){
-        if (idx == s.size()){
+    bool pal[17][17];
+    // int dp[17];
+    void helper(int i, vector<string>& temp, string& s, vector<vector<string>>& ans){
+        if (i >= s.size()){
             ans.push_back(temp);
             return;
         }
 
-        for (int i = idx; i < s.size(); i++){
-            string str = s.substr(idx, i-idx+1);
-            if (palindrome(str)){
+        string str = "";
+        for (int k = i; k < s.size(); k++){
+            str += s[k];
+            if (pal[i][k]){
                 temp.push_back(str);
-                helper(i+1, temp, s, ans);
+                helper(k+1, temp, s, ans);
                 temp.pop_back();
             }
         }
@@ -25,9 +21,23 @@ public:
     vector<vector<string>> partition(string s) {
         int n = s.size();
         vector<vector<string>>ans;
+        memset(pal, -1, sizeof(pal));
+        // memset(dp, -1, sizeof(dp));
+
+        for (int len = 1; len <= n; len++){
+            for (int i = 0; i+len-1 < n; i++){
+                int j = i+len-1;
+                if (i == j) pal[i][i] = true;
+                else if (i+1 == j) pal[i][j] = (s[i] == s[j]);
+                else {
+                    if (s[i] == s[j] && pal[i+1][j-1]) pal[i][j] = true;
+                    else pal[i][j] = false;
+                }
+            }
+        }
+
         vector<string>temp;
         helper(0, temp, s, ans);
-
         return ans;
     }
 };
