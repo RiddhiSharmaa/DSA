88fe11264f22f36b06349725ed1a787d63057b6d
RiddhiSharmaa <Riddhisharma9900@gmail.com>
2026-07-27 14:19:58 +0530

    Time: 15 ms (85.83%) | Memory: 85.8 MB (25.50%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,35 +1,45 @@
 class Solution {
 public:
-    int largestRectangleArea(vector<int>& heights) {
+    void helperPSE(vector<int>& heights, vector<int>& pse){
+            int n = heights.size();
+            stack<int>st;
+
+            for (int i = 0; i < n; i++){
+                while (!st.empty() && heights[i] <= heights[st.top()]){
+                    st.pop();
+                }
+                if (!st.empty()) pse[i] = st.top();
+                st.push(i);
+            }
+
+            return;
+        }
+
+    void helperNSE(vector<int>& heights, vector<int>& nse){
         int n = heights.size();
-        vector<int>nse(n, n);
         stack<int>st;
 
         for (int i = n-1; i >= 0; i--){
-            while (!st.empty() && heights[st.top()] >= heights[i]){
+            while (!st.empty() && heights[i] <= heights[st.top()]){
                 st.pop();
             }
             if (!st.empty()) nse[i] = st.top();
             st.push(i);
         }
 
-        vector<int>pse(n, -1);
-        stack<int>st2;
+        return;
+    }
+    int largestRectangleArea(vector<int>& heights) {
+        int n = heights.size();
+        vector<int>nse(n, n), pse(n, -1);
+        helperNSE(heights, nse);
+        helperPSE(heights, pse);
+        int ans = 0;
 
         for (int i = 0; i < n; i++){
-            while (!st2.empty() && heights[st2.top()] >= heights[i]){
-                st2.pop();
-            }
-            if (!st2.empty()) pse[i] = st2.top();
-            st2.push(i);
+            ans = max(ans, (nse[i] - pse[i] - 1) * heights[i]);
         }
 
-        int maxArea = INT_MIN;
-        for (int i = 0; i < n; i++){
-            int area = ((nse[i]-1) - (pse[i]+1) + 1) * heights[i];
-            maxArea = max(maxArea, area);
-        }
-
-        return maxArea;
+        return ans;
     }
 };
